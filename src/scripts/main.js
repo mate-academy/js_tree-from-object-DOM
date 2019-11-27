9a1d72a11804d62241b5315ea4195e9d368252e8
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-11-28 00:07:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+    ul.append(li);
+    createTree(ul, data[key]);
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
