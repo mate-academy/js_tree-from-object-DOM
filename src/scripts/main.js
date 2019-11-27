d13eeb7fe818b458c0101952264174d1785edf25
cs-cognitome <gaya.scienza.v@gmail.com>
2019-11-27 23:05:57 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.innerHTML = getList(data);
+}
+
+function getList(data) {
+  let list = '';
+
+  for (const key in data) {
+    list += `<li> ${key} ${getList(data[key])} </li>`;
+  }
+
+  if (list) {
+    return `<ul> ${list} </ul>`;
+  } else {
+    return '';
+  }
 }
 
 createTree(tree, food);
