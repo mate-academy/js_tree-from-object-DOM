269bd1f1b41393331d736c2fa7497cef2ccbe373
Innocent1312 <anthonydev33@gmail.com>
2019-11-27 14:05:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,18 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+const listGenerator = (data) => {
+  let li = '';
+
+  for (const item in data) {
+    li += `<li> ${item} ${listGenerator(data[item])} </li>`;
+  }
+
+  return li ? `<ul> ${li} </ul>` : '';
+};
+
+const createTree = (element, data) => {
+  element.innerHTML = listGenerator(data);
+};
 
 createTree(tree, food);
