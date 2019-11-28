6f2588a770f3b698f1bec8712db76ed1a6706cb4
natya2203 <nastya_2203@ukr.net>
2019-11-28 09:51:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.innerHTML = create(data);
+}
+
+function create(data) {
+  let li = '';
+
+  for (const dish in data) {
+    if (data.hasOwnProperty(dish)) {
+      li += `<li>${dish}${create(data[dish])}</li>`;
+    }
+  }
+
+  return `<ul>${li}</ul>`;
 }
 
 createTree(tree, food);
