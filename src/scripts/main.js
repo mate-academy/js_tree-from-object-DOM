fd79d96e5dc958e2f7f0613126579f35369ecd59
vadim-os <osinsky_vadim@yahoo.com>
2019-11-28 12:40:33 +0200

    solurion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+  element.innerHTML = createList(data);
+};
+
+function createList(obj) {
+  let list = '';
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (value) {
+      list = list + `<li>${key}${createList(value)}</li>`;
+    };
+  }
+
+  return `<ul>${list}</ul>`;
+};
 
 createTree(tree, food);
