e4d82930da730be724bbe55ea9c26cf0663b16d2
Senya Volkov <formobileprima@gmail.com>
2020-02-04 20:30:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,26 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (let i = 0; i < Object.keys(data).length; i++) {
+    const li = document.createElement('li');
+
+    li.innerHTML = Object.keys(data)[i];
+    ul.appendChild(li);
+
+    // console.log(Object.keys(data[Object.keys(data)[i]]));
+    // Делаем проверку на наличие ключей если их нет,
+    // то мы не добавляем через appendChild, ибо у нас ничего нет в ul,
+    // а пустой ul нам не нужен, для наглядности разкоментить console.log
+    if (Object.keys(data[Object.keys(data)[i]]).length) {
+      const nextIteration = createTree(element, data[Object.keys(data)[i]]);
+
+      ul.appendChild(nextIteration);
+    }
+  }
+
+  return ul;
 }
 
-createTree(tree, food);
+tree.appendChild(createTree(tree, food));
