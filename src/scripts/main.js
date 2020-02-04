599d2a7c25dd7172d8e79cc26858e2f5133ae7b4
Actireon <eheieiei37@gmail.com>
2020-02-04 22:35:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,51 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const keys = Object.keys(data);
+  const keys2 = [];
+  const keys3 = [];
+  const keys4 = [];
+
+  for (const key in data[keys[0]]) {
+    keys2.push(key);
+  }
+
+  for (const key in data[keys[1]]) {
+    keys3.push(key);
+    keys4.push(Object.keys(data[keys[1]][key]));
+  }
+
+  element.innerHTML = `
+    <ul>
+      <li>${keys[0]}
+        <ul>
+          ${keys2.map(key => `
+            <li>${key}</li>
+          `).join('')}
+        </ul>
+      </li>
+
+      <li>${keys[1]}
+        <ul>
+          <li>${keys3[0]}
+            <ul>
+              ${keys4[0].map(key => `
+                <li>${key}</li>
+              `).join('')}
+            </ul>
+          </li>
+
+          <li>${keys3[1]}
+            <ul>
+              ${keys4[1].map(key => `
+                <li>${key}</li>
+              `).join('')}
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  `;
 }
 
 createTree(tree, food);
