5281329ff1aa11385d4491bc43065c1339f899fa
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-04 22:20:07 +0200

    Tree from object DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const keys = Object.keys(data);
+
+  const ul = document.createElement('ul');
+
+  ul.className = 'food__list';
+
+  element.append(ul);
+
+  for (const key of keys) {
+    const li = document.createElement('li');
+
+    li.className = 'food__item';
+    li.textContent = key;
+    ul.append(li);
+
+    if (typeof data[key] === 'object') {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
