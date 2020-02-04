45eac31ec201cff2220f8f3f60efc759823e2a0d
Olexander <kravchenko.sania@gmail.com>
2020-02-04 18:03:14 +0200

    attempt 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  function returnElement(obj) {
+    let str = '';
+
+    for (const key in obj) {
+      if (Object.keys(obj[key]).length === 0) {
+        str += `<li>${key}</li>`;
+      } else {
+        str += `<li>${key}<ul>${returnElement(obj[key])}</ul></li>`;
+      }
+    }
+
+    return str;
+  }
+
+  element.innerHTML = `<ul>${returnElement(data)}</ul>`;
 }
 
 createTree(tree, food);
