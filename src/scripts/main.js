fc715bd01e2f991345cb3499a325f61dc3efa76b
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-04 17:12:50 +0200

    on review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,9 +20,48 @@ const food = {
 };
 
 const tree = document.querySelector('#tree');
+const foodList = Object.keys(food);
+const meatList = Object.keys(food['Meat']);
+const fruitList = Object.keys(food['Fruit']);
+const redFruit = Object.keys(food['Fruit']['Red']);
+const yellowFruit = Object.keys(food['Fruit']['Yellow']);
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  tree.innerHTML = `
+    <ul class="food">
+      ${foodList.map(item => `
+        <li class="food__item">${item}<ul class="food-type"></ul></li>
+      `).join('')}
+    </ul>
+  `;
+
+  const foodTypeList = document.querySelectorAll('.food-type');
+
+  foodTypeList[0].innerHTML = `
+    ${meatList.map(item => `
+      <li class="meat__item">${item}</li>
+    `).join('')}
+  `;
+
+  foodTypeList[1].innerHTML = `
+    ${fruitList.map(item => `
+      <li class="fruit__color">${item}<ul class="fruit-type__list"></ul></li>
+    `).join('')}
+  `;
+
+  const fruitTypeList = document.querySelectorAll('.fruit-type__list');
+
+  fruitTypeList[0].innerHTML = `
+    ${redFruit.map(item => `
+      <li class="meat__item">${item}</li>
+    `).join('')}
+  `;
+
+  fruitTypeList[1].innerHTML = `
+    ${yellowFruit.map(item => `
+      <li class="fruit__color">${item}</li>
+    `).join('')}
+  `;
 }
 
 createTree(tree, food);
