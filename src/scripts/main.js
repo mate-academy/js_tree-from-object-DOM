f8645e6484c8d823926bf4df4f6fb83167b4517a
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-02-05 17:36:43 +0200

    tree from obj DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    ul.append(li);
+    li.textContent = key;
+
+    if (Object.values(data[key]).length > 0) {
+      createTree(li, data[key]);
+    };
+  };
 }
 
 createTree(tree, food);
