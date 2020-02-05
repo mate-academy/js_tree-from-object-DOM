21f48ff4478d828c8d47e6c6416bc1d25811c360
Actireon <eheieiei37@gmail.com>
2020-02-05 16:01:18 +0200

    refactoring code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,49 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  const keys = Object.keys(data);
-  const keys2 = [];
-  const keys3 = [];
-  const keys4 = [];
+  function createElement(object) {
+    let li = '';
 
-  for (const key in data[keys[0]]) {
-    keys2.push(key);
+    for (const key in object) {
+      !Object.keys(object[key])
+        ? li += `<li>${key}</li>`
+        : li += `<li>${key}<ul>${createElement(object[key])}</ul></li>`;
+    }
+
+    return li;
   }
 
-  for (const key in data[keys[1]]) {
-    keys3.push(key);
-    keys4.push(Object.keys(data[keys[1]][key]));
-  }
-
-  function map(arr) {
-    return arr.map(key => `<li>${key}</li>`).join('');
-  }
-
-  element.innerHTML = `
-    <ul>
-      <li>${keys[0]}
-        <ul>
-          ${map(keys2)}
-        </ul>
-      </li>
-
-      <li>${keys[1]}
-        <ul>
-          <li>${keys3[0]}
-            <ul>
-              ${map(keys4[0])}
-            </ul>
-          </li>
-
-          <li>${keys3[1]}
-            <ul>
-              ${map(keys4[1])}
-            </ul>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  `;
+  element.innerHTML = `<ul>${createElement(data)}</ul>`;
 }
 
 createTree(tree, food);
