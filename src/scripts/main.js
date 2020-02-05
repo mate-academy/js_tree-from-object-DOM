5b701d0d61963f086a3b4ab72ebf868950ec76f9
Dan Denysovets <alen250299@gmail.com>
2020-02-05 16:38:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,10 +19,51 @@ const food = {
   },
 };
 
+const food2 = {
+  'Meat': {
+    'Pork': {
+      'Fruit': {
+        'Red': {
+          'Cherry': {
+            'Beef': {
+              'Pineapple': {
+                'Banana': {
+                  'Strawberry': {
+                    'Cherry': {},
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
 const tree = document.querySelector('#tree');
+const title = document.createElement('h1');
+
+title.innerHTML = 'Recursion object list';
+
+document.body.append(title);
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+  const list = document.createElement('ul');
+
+  element.append(list);
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.innerHTML = key;
+    list.append(listItem);
+
+    if (Object.values(data[key]).length > 0) {
+      createTree(listItem, data[key]);
+    }
+  };
+};
 
 createTree(tree, food);
+createTree(tree, food2);
