2100a5c88da9231d082d5215d16057b273026931
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-05 18:45:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,17 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.innerHTML = createList(data);
 }
+
+const createList = (data) => {
+  let li = '';
+
+  for (const key in data) {
+    li += `<li> ${key} ${createList(data[key])}</li>`;
+  }
+
+  return li ? `<ul>${li}</ul>` : '';
+};
 
 createTree(tree, food);
