085120677096462fafaf5778fc8e3200f8bb914a
MykolaMak <makolamak@gmail.com>
2020-04-14 23:24:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  element.append(list);
+
+  for (const el in data) {
+    const listElement = document.createElement('li');
+
+    listElement.innerText = el;
+    list.append(listElement);
+
+    if (Object.keys(data[el]).length !== 0) {
+      createTree(listElement, data[el]);
+    }
+  }
 }
 
 createTree(tree, food);
