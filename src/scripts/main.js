49472d3f91e0af0d734ad10f18e548859ea18ebe
kochatkov <vlad.kochatkov@gmail.com>
2020-04-14 19:10:26 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,17 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  ul.append('ul');
+
+  for (const item in data) {
+    const li = document.createElement('li');
+
+    li.textContent = item;
+    ul.append('li');
+    createTree(element, data[item]);
+  }
 }
 
 createTree(tree, food);
