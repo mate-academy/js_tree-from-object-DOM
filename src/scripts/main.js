91606382daf16ceadf0da8b394b5ad12aaebfbcd
kate <kate@test.com>
2020-04-14 18:33:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,21 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+    ul.append(li);
+
+    if (Object.values(data[key])) {
+      createTree(li, data[key]);
+    };
+  };
 }
 
 createTree(tree, food);
