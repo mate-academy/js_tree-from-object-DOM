2a155564ba01b1a9052ba4c717e4d0494fbc585f
Nastia-rosy <nastiiarosy@gmail.com>
2020-04-15 15:00:53 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,23 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const keysArray = Object.keys(data);
+
+  if (keysArray.length === 0) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key of keysArray) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    createTree(li, data[key]);
+    ul.append(li);
+  }
 }
 
 createTree(tree, food);
