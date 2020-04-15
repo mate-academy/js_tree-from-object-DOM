33b9178891faa0a6005eda47297976f9c3784984
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-15 14:13:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    ul.append(li);
+    li.textContent = key;
+
+    if (typeof data[key] === 'object') {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
