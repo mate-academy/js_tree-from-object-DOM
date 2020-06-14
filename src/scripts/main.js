aa82de750a1680e23140f3018dbba5f813fa422d
solaryasha <solaryasha@gmail.com>
2020-06-14 18:38:18 +0300

    My first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const { utils } = require("stylelint");
+
 const food = {
   'Meat': {
     'Pork': {},
@@ -22,7 +24,27 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.append(createDom(data));
 }
+
+const createDom = data => {
+  const list = document.createElement('ul');
+
+  Object.keys(data).forEach(element => {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = element;
+
+    if (Object.keys(data[element]).length) {
+      const childrenList = createDom(data[element]);
+
+      listItem.append(childrenList);
+    }
+
+    list.append(listItem);
+  });
+
+  return list;
+};
 
 createTree(tree, food);
