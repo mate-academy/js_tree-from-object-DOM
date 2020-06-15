31001caa9e7215dab321b0fa70b14cb4b388849f
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-15 15:35:56 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  if (Object.keys(data).length > 0) {
+    element.append(list);
+  };
+
+  for (const [key, value] of Object.entries(data)) {
+    const item = document.createElement('li');
+
+    item.textContent = key;
+
+    list.append(item);
+
+    createTree(item, value);
+  };
 }
 
 createTree(tree, food);
