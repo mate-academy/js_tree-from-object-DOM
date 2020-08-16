d7e33de5553297185ef128d9c28858594a135e61
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-16 17:00:27 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,20 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const i in data) {
+    const li = document.createElement('li');
+
+    li.textContent = i;
+    ul.append(li);
+
+    if (Object.keys(data[i]).length > 0) {
+      createTree(ul, data[i]);
+    }
+  }
 }
 
 createTree(tree, food);
