fa710c70a3f85ab5fb39750957041597d51886b3
oggyrock <steelo.sun@gmail.com>
2020-08-22 18:12:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,17 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (Object.keys(data).length) {
+    element.insertAdjacentHTML('beforeend', `<ul></ul>`);
+
+    const list = element.querySelectorAll('ul');
+    const ul = list[list.length - 1];
+
+    for (const li in data) {
+      ul.insertAdjacentHTML('beforeend', `<li>${li}</li>`);
+      createTree(ul, data[li]);
+    }
+  }
 }
 
 createTree(tree, food);
