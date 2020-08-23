60f0c490e7a824c6335745f39f1cb904ee2130e3
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-23 22:54:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,12 +17,35 @@ const food = {
     },
 
   },
+
+  'Test Without Elements': {},
 };
 
 const tree = document.querySelector('#tree');
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+function createTree(container, data) {
+  const element = document.createElement('ul');
+
+  container.append(element);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerText = key;
+    element.append(li);
+
+    if (Object.keys(data[key]).length) { // if element has subElements
+      const liOfChildren = document.createElement('li');
+
+      liOfChildren.style.listStyleType = 'none';
+      element.append(liOfChildren);
+
+      const ul = document.createElement('ul');
+
+      liOfChildren.append(ul);
+      createTree(ul, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
