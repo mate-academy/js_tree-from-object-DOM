d4b8beaa6677d2b4c94bd32495e293a499c6d719
Mikle25 <mikle2593@gmail.com>
2020-08-24 09:54:07 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const item in data) {
+    const itemList = document.createElement('li');
+
+    itemList.textContent = item;
+    list.append(itemList);
+
+    if (typeof data[item] === 'object') {
+      createTree(itemList, data[item]);
+    }
+  }
+
+  element.append(list);
 }
 
 createTree(tree, food);
