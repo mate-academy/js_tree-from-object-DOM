f563dc424e5c654eb4e9e0b9f4c406d17fdd4c3f
nanccyy <lidiapriymak@gmail.com>
2020-08-24 16:57:21 +0800

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    element.insertAdjacentHTML('beforeend', `
+      <ul>
+        <li class="${key}">${key}</li>
+      </ul>
+    `);
+
+    if (key) {
+      createTree(document.querySelector(`.${key}`), data[key]);
+    } else {
+      return;
+    }
+  }
 }
 
 createTree(tree, food);
