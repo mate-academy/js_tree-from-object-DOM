8a468a5373f15c87cdc3a494b5e31ac120761af9
Alona <aionovaionova@gmail.com>
2020-10-05 17:51:29 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,26 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    if (typeof data[key] === 'object') {
+      const listInList = document.createElement('li');
+
+      listInList.textContent = key;
+      element.appendChild(listInList);
+
+      const listInListItems = document.createElement('ul');
+
+      listInList.appendChild(listInListItems);
+
+      createTree(listInListItems, data[key]);
+    } else {
+      const listItem = document.createElement('li');
+
+      listItem.textContent = data[key];
+
+      element.appendChild(listItem);
+    }
+  }
 }
 
 createTree(tree, food);
