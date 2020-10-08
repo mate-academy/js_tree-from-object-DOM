da7fad5ab66ab9ce43316a88118a21d6ec0d7779
VoduDay <andriy.maniak@gmail.com>
2020-10-08 18:18:12 +0300

    tree from object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const uList = document.createElement('ul');
+
+  for (const meal in data) {
+    const list = document.createElement('li');
+    const nextList = data[meal];
+
+    list.textContent = meal;
+    uList.append(list);
+
+    if (Object.keys(nextList).length) {
+      createTree(list, nextList);
+    }
+  }
+
+  element.append(uList);
 }
 
 createTree(tree, food);
