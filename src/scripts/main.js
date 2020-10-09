353d3e936aefe9d55eca1b6ff5968d817285f9ac
VoduDay <andriy.maniak@gmail.com>
2020-10-09 10:20:28 +0300

    fix name of variable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,21 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  const uList = document.createElement('ul');
+  const unorderedList = document.createElement('ul');
 
-  for (const meal in data) {
+  for (const key in data) {
     const list = document.createElement('li');
-    const nextList = data[meal];
+    const nextList = data[key];
 
-    list.textContent = meal;
-    uList.append(list);
+    list.textContent = key;
+    unorderedList.append(list);
 
     if (Object.keys(nextList).length) {
       createTree(list, nextList);
     }
   }
 
-  element.append(uList);
+  element.append(unorderedList);
 }
 
 createTree(tree, food);
