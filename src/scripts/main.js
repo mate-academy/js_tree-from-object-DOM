084449db439dba326349a162557958ea17e6a182
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-13 22:24:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const group in data) {
+    const li = document.createElement('li');
+
+    li.textContent = group;
+    ul.append(li);
+
+    if (Object.keys(data[group])) {
+      createTree(li, data[group]);
+    }
+  }
 }
 
 createTree(tree, food);
