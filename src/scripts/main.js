b0ab2cd667ceec6ebd709cab9bffeee20914f97a
Anton <office.sklyarov@gmail.com>
2020-10-15 15:06:12 +0300

    added lists tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,22 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const newList = document.createElement('ul');
+
+  for (const key in data) {
+    const newListItem = document.createElement('li');
+
+    newListItem.append(key);
+
+    newList.append(newListItem);
+    element.append(newList);
+
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(newList, data[key]);
+    } else {
+      continue;
+    }
+  }
 }
 
 createTree(tree, food);
