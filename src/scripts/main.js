89ab06f0e81eb743f6ab52e992eda69016bcb4d9
Nikolay Filiuk <Filiuk98@gmail.com>
2020-10-15 18:33:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const item = document.createElement('li');
+
+    item.innerText = key;
+    element.append(list);
+    list.append(item);
+
+    if (Object.entries(data[key]).length) {
+      createTree(item, data[key]);
+    }
+  }
+};
 
 createTree(tree, food);
