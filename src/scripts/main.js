fedcc237afd84d3127b5490fb9d87a81a0c87408
milla-romankova <milla-romankova@gmail.com>
2020-10-16 14:55:12 +0300

    remove append(list) from the loop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,12 +24,13 @@ const tree = document.querySelector('#tree');
 function createTree(element, data) {
   const list = document.createElement('ul');
 
+  element.append(list);
+
   for (const key in data) {
     const listItem = document.createElement('li');
 
     listItem.innerText = key;
     list.append(listItem);
-    element.append(list);
 
     if (Object.keys(data[key]).length) {
       createTree(listItem, data[key]);
