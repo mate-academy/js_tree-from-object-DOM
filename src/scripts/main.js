6a23c39ebaac58185eaf288e3ec769a3846409f2
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-17 22:19:35 +0300

    tree from object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  element.append(list);
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    list.append(listItem);
+    listItem.textContent = key;
+
+    createTree(listItem, data[key]);
+  }
 }
 
 createTree(tree, food);
