d6823f5af7cec14c746cf6ad088e6b223e30f61c
Olga Rubalska <olga.anikeieva@gmail.com>
2020-11-15 21:07:34 +0200

    Created function `createTree(element, data)` that creates a nested list of `ul/li` from an object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const uL = document.createElement('ul');
+
+  element.append(uL);
+
+  for (const prop in data) {
+    if (typeof (data[prop]) === 'object') {
+      const lI = document.createElement('li');
+
+      lI.innerHTML = prop;
+      uL.append(lI);
+      createTree(uL, data[prop]);
+    } else {
+      return;
+    }
+  }
 }
 
 createTree(tree, food);
