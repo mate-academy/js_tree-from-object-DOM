dba8c83af1736f8db9b7e248a9526f504e09d95d
y.kostenko <y.kostenko@mate.academy>
2020-12-09 19:02:46 +0200

    custom command improved?

--- cypress/integration/treeFromObject.spec.js
+++ cypress/integration/treeFromObject.spec.js
@@ -1,7 +1,19 @@
 'use strict';
 
-Cypress.Commands.add('haveTwoChildren', (value) => {
-  cy.contains(value).children().children().should('have.length', 2);
+Cypress.Commands.add('haveChildren', {
+  prevSubject: true,
+}, (subject, value, number) => {
+  if (number > 0) {
+    cy.contains(value).children().children().should('have.length', number);
+  }
+
+  if (number === 0) {
+    cy.contains(value).children().should('have.length', number);
+  }
+
+  if (!value) {
+    cy.wrap(subject).children().children().should('have.length', 2);
+  }
 });
 
 describe('Tree from object app', () => {
@@ -10,25 +22,25 @@ describe('Tree from object app', () => {
   });
 
   it('should have 1 unordered list with 2 list items', () => {
-    cy.get('#tree').children().children().should('have.length', 2);
+    cy.get('#tree').haveChildren();
   });
 
   it('should have an <ul> with 2 list items in first list item', () => {
-    cy.haveTwoChildren('Drink');
+    cy.get('#tree').haveChildren('Drink', 2);
   });
 
   it('should have an <ul> with 2 list items in second list item', () => {
-    cy.haveTwoChildren('Fruit');
+    cy.get('#tree').haveChildren('Fruit', 2);
   });
 
   it('should have 2 <li> in each <ul> in Fruits', () => {
-    cy.haveTwoChildren('Red');
-    cy.haveTwoChildren('Yellow');
+    cy.get('#tree').haveChildren('Red', 2);
+    cy.get('#tree').haveChildren('Yellow', 2);
   });
 
   it(`shouldn't have <ul> elements in 3rd children`, () => {
-    cy.contains('Cherry').children().should('have.length', 0);
-    cy.contains('Banana').children().should('have.length', 0);
-    cy.contains('Wine').children().should('have.length', 0);
+    cy.get('#tree').haveChildren('Cherry', 0);
+    cy.get('#tree').haveChildren('Banana', 0);
+    cy.get('#tree').haveChildren('Wine', 0);
   });
 });
