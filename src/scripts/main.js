188f49a0ae10cd5d9fb63ee6ccf8f0baed947bfc
MarchD <dubchmar@gmail.com>
2021-01-05 14:10:40 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,15 +24,18 @@ const tree = document.querySelector('#tree');
 function createTree(element, data) {
   const ul = document.createElement('ul');
 
-  element.append(ul);
-
   Object.keys(data).forEach(topic => {
     const li = document.createElement('li');
 
     ul.append(li);
     li.innerText = topic;
-    createTree(ul, data[topic]);
+
+    if (Object.keys(data[topic]).length > 0) {
+      createTree(li, data[topic]);
+    }
   });
+
+  element.append(ul);
 }
 
 createTree(tree, food);
