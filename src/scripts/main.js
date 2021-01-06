2a6472f8d0025d68d0277e867c23c75fa89876f2
Oleksii Spalylo <white.rh42@gmail.com>
2021-01-06 19:04:28 +0200

    recursion solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (Object.keys(data).length === 0) {
+      return;
+    }
+
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    createTree(li, data[key]);
+    ul.append(li);
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
