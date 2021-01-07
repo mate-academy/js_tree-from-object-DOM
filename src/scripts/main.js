3be395ab8952ae538355515320973200d8e627c9
gorzer <gorzer.rostyslav@gmail.com>
2021-01-07 13:29:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,56 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+// function createTree(element, data) {
+//   element.append(createTreeDOM(data));
+// }
+
+// function createTreeDOM(obj) {
+//   if (!Object.keys(obj).length) {
+//     return undefined;
+//   }
+
+//   let ul = document.createElement('ul');
+
+//   for (const key in obj) {
+//     const li = document.createElement('li');
+
+//     li.innerHTML = key;
+
+//     const childrenUl = createTreeDOM(obj[key]);
+
+//     if (childrenUl) {
+//       li.append(childrenUl);
+//     }
+
+//     ul.append(li);
+//   }
+
+//   return ul;
+// }
+
+// createTree(tree, food);
+
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (!Object.entries(data[key]).length) {
+      ul.insertAdjacentHTML('beforeend', `
+        <li>${key}</li>`);
+
+      element.append(ul);
+    } else {
+      const li = document.createElement('li');
+
+      li.innerHTML = key;
+
+      ul.append(li);
+
+      element.append(ul);
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
