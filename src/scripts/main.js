31f500a2b3f624c25de9c0286950ca4638f59839
morozoleh <olegmoroz93@gmail.com>
2021-02-24 20:59:33 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,11 +24,10 @@ const tree = document.querySelector('#tree');
 function createTree(element, data) {
   const list = document.createElement('ul');
 
-  element.append(list);
-
   for (const key in data) {
     const item = document.createElement('li');
 
+    element.append(list);
     list.append(item);
     item.textContent = key;
 
