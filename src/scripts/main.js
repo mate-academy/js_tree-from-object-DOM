bf318bf01d668a5217c924cb12541e81f6e054b2
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-05 14:48:13 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,33 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (Object.keys(element).length !== 0) {
+    const protoUl = document.createElement('ul');
+
+    for (const key in element) {
+      const protoLi = document.createElement('li');
+
+      data.append(protoUl);
+      protoUl.append(protoLi);
+      protoLi.textContent = key;
+
+      if (Object.keys(element[key]).length !== 0) {
+        createTree(element[key], protoLi);
+      }
+    }
+  }
 }
+
+/* function test(element) {
+  debugger;
+  if (Object.keys(element).length !== 0) {
+    for (const key in element) {
+      console.log(key);
+      if (Object.keys(element[key]).length !== 0) {
+        test(element[key]);
+      }
+    }
+  }
+} */
 
 createTree(tree, food);
