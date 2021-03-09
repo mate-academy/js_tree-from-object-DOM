6fafa62068009658129c1eb71838ddcc7065e7e5
Ping <4ivrik@ukr.net>
2021-03-09 16:14:28 +0200

    objects keys check added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,7 +29,7 @@ function createTree(element, data) {
     item.innerText = key;
     list.append(item);
 
-    if (data[key]) {
+    if (Object.keys(data).length !== 0) {
       createTree(item, data[key]);
     }
     element.append(list);
