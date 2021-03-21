ffa4ff05c7ace894c081a97e8fd5d813acf2feb2
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-21 19:35:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function isNotEmpty(obj) {
+  return Object.keys(obj).length !== 0;
+}
+
 const food = {
   'Drink': {
     'Wine': {},
@@ -22,7 +26,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+
+    list.append(listItem);
+
+    if (isNotEmpty(data[key])) {
+      createTree(listItem, data[key]);
+    }
+  }
+  element.append(list);
 }
 
 createTree(tree, food);
