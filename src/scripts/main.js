31f9dd3db7e1b05e4ffab3640cb944486ec7cfa8
olena <melolena33@gmail.com>
2021-03-22 14:22:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,24 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (element) {
+    const ulElem = document.createElement('ul');
+
+    element.append(ulElem);
+
+    for (const type in data) {
+      const liElem = document.createElement('li');
+
+      liElem.textContent = type;
+      ulElem.append(liElem);
+
+      const innerData = data[type];
+
+      if (Object.keys(innerData).length > 0) {
+        createTree(liElem, innerData);
+      }
+    }
+  }
 }
 
 createTree(tree, food);
