cb4d2036de753e87df717a64d6de11d400e5fe2a
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-24 20:08:01 +0200

    Created nested list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,25 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  function createList(dataList) {
+    let string = '';
+
+    for (const key in dataList) {
+      if (Object.keys(dataList[key]).length !== 0) {
+        string += `<li>${key}<ul>${createList(dataList[key])}</ul></li>`;
+      } else {
+        string += `<li>${key}</li>`;
+      }
+    }
+
+    return string;
+  }
+
+  list.insertAdjacentHTML('beforeend', createList(data));
+
+  element.append(list);
 }
 
 createTree(tree, food);
