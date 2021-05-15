0a4f85064279a941dfd43d5c170aa0ca7e2c976b
Ihor Karpyn <igor82200@gmail.com>
2021-05-15 11:35:59 +0300

    added function createTree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,27 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+function createTree(element, dataFood) {
+  const createEl = (data) => {
+    const ul = document.createElement('ul');
+
+    for (const dataKey in data) {
+      const li = document.createElement('li');
+
+      li.innerText = dataKey;
+
+      if (Object.keys(data[dataKey]).length === 0) {
+        ul.append(li);
+      } else {
+        li.append(createEl(data[dataKey]));
+        ul.append(li);
+      }
+    }
+
+    return ul;
+  };
+
+  element.append(createEl(dataFood));
 }
 
 createTree(tree, food);
