0949f3ba34a2ad640a6419ee16d86885deea08e3
Tolik <tolikdolinskiy@gmail.com>
2021-06-21 10:28:45 +0300

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,39 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  function createUl(elem, parentUl) {
+    for (const key in elem) {
+      const li = document.createElement('li');
+      const ulNested = document.createElement('ul');
+
+      if (Object.keys(elem[key]).length === 0) {
+        li.innerHTML = key;
+        parentUl.append(li);
+
+        continue;
+      }
+
+      li.innerHTML = key;
+      li.append(ulNested);
+      parentUl.append(li);
+      createUl(elem[key], ulNested);
+    }
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const k in data) {
+    const ulNested = document.createElement('ul');
+    const li = document.createElement('li');
+
+    li.innerHTML = k;
+    li.append(ulNested);
+    ul.append(li);
+    createUl(food[k], ulNested);
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
