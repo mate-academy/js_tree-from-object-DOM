655baca685ef0b018603c4aaafd3a58ef019b837
Anton Forzun <anton_forzun@ukr.net>
2021-09-22 14:34:14 +0300

    tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+  const key = Object.keys(data);
+  const values = Object.values(data);
+
+  for (let i = 0; i < key.length; i++) {
+    const lists = document.createElement('li');
+
+    lists.textContent = key[i];
+    list.append(lists);
+
+    if (Object.keys(values[i]).length !== 0) {
+      createTree(list, values[i]);
+    }
+  }
+  element.append(list);
 }
 
 createTree(tree, food);
