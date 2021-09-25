44d953bb6d1289a775962b67244b6a82bf36d533
bohdan_kochetov <bodja1208@gmail.com>
2021-09-25 23:48:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,31 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.append(create(data));
+}
+
+function create(data) {
+  const ul = document.createElement('ul');
+
+  if (Object.keys(data).length === undefined) {
+    return;
+  }
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+
+    const child = create(data[key]);
+
+    if (child) {
+      li.append(child);
+    }
+
+    ul.append(li);
+  }
+
+  return ul;
 }
 
 createTree(tree, food);
