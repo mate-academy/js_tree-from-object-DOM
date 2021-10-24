50358b7be133463d45297be5fc62926420afb5e2
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-10-24 13:55:47 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,54 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const mainArray = Object.keys(data);
+
+  const mainUl = document.createElement('ul');
+
+  for (const key of mainArray) {
+    const firstLevelLi = document.createElement('li');
+
+    firstLevelLi.id = key;
+    firstLevelLi.textContent = key;
+    mainUl.append(firstLevelLi);
+
+    const firstArray = Object.keys(data[key]);
+    const innerUl = document.createElement('ul');
+
+    firstArray.map(item => {
+      const li = document.createElement('li');
+
+      li.id = item;
+      li.textContent = item;
+      innerUl.append(li);
+    });
+
+    firstLevelLi.append(innerUl);
+  }
+
+  element.append(mainUl);
+
+  const groups = Object.values(data);
+
+  groups.map(group => {
+    const secondArray = Object.entries(group);
+
+    secondArray.map(item => {
+      if (Object.keys(item[1]).length > 0) {
+        const parentLi = document.getElementById(item[0]);
+        const innerUl = document.createElement('ul');
+        const entries = Object.keys(item[1]);
+
+        for (const product of entries) {
+          const li = document.createElement('li');
+
+          li.textContent = product;
+          innerUl.append(li);
+        }
+        parentLi.append(innerUl);
+      }
+    });
+  });
 }
 
 createTree(tree, food);
