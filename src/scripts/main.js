4304fa6e983402fd3035410f879d0ed0008bcb89
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-10-25 19:53:52 +0300

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,54 +22,16 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  const mainArray = Object.keys(data);
-
-  const mainUl = document.createElement('ul');
-
-  for (const key of mainArray) {
-    const firstLevelLi = document.createElement('li');
-
-    firstLevelLi.id = key;
-    firstLevelLi.textContent = key;
-    mainUl.append(firstLevelLi);
-
-    const firstArray = Object.keys(data[key]);
-    const innerUl = document.createElement('ul');
-
-    firstArray.map(item => {
-      const li = document.createElement('li');
-
-      li.id = item;
-      li.textContent = item;
-      innerUl.append(li);
-    });
-
-    firstLevelLi.append(innerUl);
+  for (let key in data) {
+    let ul = document.createElement('ul');
+    let li = document.createElement('li');
+    li.textContent = key;
+    if (Object.keys(data[key]).length) {
+      createTree(li, data[key]);
+    }
+    ul.append(li);
+    element.append(ul);
   }
-
-  element.append(mainUl);
-
-  const groups = Object.values(data);
-
-  groups.map(group => {
-    const secondArray = Object.entries(group);
-
-    secondArray.map(item => {
-      if (Object.keys(item[1]).length > 0) {
-        const parentLi = document.getElementById(item[0]);
-        const innerUl = document.createElement('ul');
-        const entries = Object.keys(item[1]);
-
-        for (const product of entries) {
-          const li = document.createElement('li');
-
-          li.textContent = product;
-          innerUl.append(li);
-        }
-        parentLi.append(innerUl);
-      }
-    });
-  });
 }
 
-createTree(tree, food);
+createTree(tree, food)
