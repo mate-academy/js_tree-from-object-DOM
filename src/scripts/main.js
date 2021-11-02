8f314ee20648254b0eb4a8e3c4fce4ec35e0b23e
FocusHuK <kirill919191kirill@gmail.com>
2021-11-02 11:38:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,36 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const DrinkFruit in data) {
+    const ulDrinkFruit = document.createElement('ul');
+    const liDrinkFruit = document.createElement('li');
+
+    element.append(ulDrinkFruit);
+    ulDrinkFruit.append(liDrinkFruit);
+    liDrinkFruit.textContent = DrinkFruit;
+
+    for (const sort in data[DrinkFruit]) {
+      const ulSort = document.createElement('ul');
+      const liSort = document.createElement('li');
+
+      liDrinkFruit.append(ulSort);
+      ulSort.append(liSort);
+      liSort.textContent = sort;
+
+      for (const fruit in data[DrinkFruit][sort]) {
+        const ulFruit = document.createElement('ul');
+        const liFruit = document.createElement('li');
+
+        ulSort.append(ulFruit);
+        ulFruit.append(liFruit);
+        liFruit.textContent = fruit;
+      }
+    }
+  }
 }
+
+// не понял как это сделать через рекурсию.
+// Если можно кусочек кода для этой задачи с примером рекурсии.
+// Потому что тут легко запутаться.
 
 createTree(tree, food);
