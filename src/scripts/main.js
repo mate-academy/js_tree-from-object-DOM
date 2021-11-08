bb2fd44b6ee97ce1805b87f8af0a35be23b42c6a
FocusHuK <kirill919191kirill@gmail.com>
2021-11-08 11:16:30 +0200

    Edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,28 +30,10 @@ function createTree(element, data) {
     ulDrinkFruit.append(liDrinkFruit);
     liDrinkFruit.textContent = DrinkFruit;
 
-    for (const sort in data[DrinkFruit]) {
-      const ulSort = document.createElement('ul');
-      const liSort = document.createElement('li');
-
-      liDrinkFruit.append(ulSort);
-      ulSort.append(liSort);
-      liSort.textContent = sort;
-
-      for (const fruit in data[DrinkFruit][sort]) {
-        const ulFruit = document.createElement('ul');
-        const liFruit = document.createElement('li');
-
-        ulSort.append(ulFruit);
-        ulFruit.append(liFruit);
-        liFruit.textContent = fruit;
-      }
+    if (Object.keys(data[DrinkFruit]) !== 0) {
+      createTree(liDrinkFruit, data[DrinkFruit]);
     }
   }
-}
-
-// не понял как это сделать через рекурсию.
-// Если можно кусочек кода для этой задачи с примером рекурсии.
-// Потому что тут легко запутаться.
+};
 
 createTree(tree, food);
