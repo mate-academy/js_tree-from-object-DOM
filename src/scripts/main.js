57a4f321cb3916b300ed69d77d06ec1da1d01aed
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2021-12-04 01:04:59 +0200

    last saved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    li.innerText = key;
+    ul.appendChild(li);
+    element.appendChild(ul);
+
+    if (Object.keys(key) !== 'undefined') {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
