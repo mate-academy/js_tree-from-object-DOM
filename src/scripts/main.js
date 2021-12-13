8db53787d54d234085547cb51cc212eed24cdeb8
Marina Gerasimenko <marigera1917@gmail.com>
2021-12-13 20:44:35 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,19 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const el in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = el;
+    ul.append(li);
+
+    if (typeof data[el] === 'object' && Object.keys(data[el]).length > 0) {
+      createTree(ul, data[el]);
+    }
+  }
+  element.append(ul);
 }
 
 createTree(tree, food);
