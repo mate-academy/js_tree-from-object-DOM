9d3811448b496bde91cc39278c6e4f9e1896f68b
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2021-12-26 13:12:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,14 +15,27 @@ const food = {
       'Banana': {},
       'Pineapple': {},
     },
-
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    li.innerText = key;
+    element.append(li);
+
+    if (Object.keys(data).length > 1) {
+      createTree(ul, data[key]);
+
+      li.append(ul);
+    }
+
+    li.append(ul);
+  }
 }
 
 createTree(tree, food);
