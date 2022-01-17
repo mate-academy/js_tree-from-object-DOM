b3301d5ee989093bd9adc93962dac1a901f7fc68
Victor Hrygorash <viktor12041991@i.ua>
2022-01-17 15:43:40 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+    element.append(ul);
+
+    if (typeof data[key] === 'object') {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
