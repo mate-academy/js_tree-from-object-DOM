3d1d102f3d3bab269a40953e529ed397f358bab0
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-31 13:36:16 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.appendChild(createTreeDom(data));
 }
 
 createTree(tree, food);
+
+function createTreeDom(obj) {
+  const ul = document.createElement('ul');
+
+  for (const key in obj) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+
+    if (Object.keys(obj[key]).length) {
+      li.appendChild(createTreeDom(obj[key]));
+    };
+
+    ul.appendChild(li);
+  }
+
+  return ul;
+}
