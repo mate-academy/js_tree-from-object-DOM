a9d1f427f7f6d7c85da4cb8acd05e3ba36ccb4d2
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-08 21:58:34 +0200

    add function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,22 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  element.innerHTML = createText(data);
+}
+
+function createText(obj) {
+  let li = '';
+  let ul;
+
+  for (const key in obj) {
+    li += '<li>' + key + createText(obj[key]) + '</li>';
+  }
+
+  if (li) {
+    ul = '<ul>' + li + '</ul>';
+  }
+
+  return ul || '';
 }
 
 createTree(tree, food);
