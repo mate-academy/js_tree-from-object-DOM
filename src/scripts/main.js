43c5012588d045cef6c0de6dfbfa33c20113ba49
Roman Kovalchuk <roma220702@gmail.com>
2022-02-10 18:11:02 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,11 +18,36 @@ const food = {
 
   },
 };
-
 const tree = document.querySelector('#tree');
-
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const obj = Object.keys(data);
+  const obj1 = Object.keys(data['Drink']);
+  const obj2 = Object.keys(data['Fruit']);
+  const obj3 = Object.keys(data['Fruit']['Red']);
+  const obj4 = Object.keys(data['Fruit']['Yellow']);
+  element.innerHTML = `
+  <ul>
+    <li>${obj[0]}</li>
+      <ul>
+        <li>${obj1[0]}</li>
+        <li>${obj1[1]}</li>
+      </ul>
+    <li>${obj[1]}</li>
+      <ul>
+        <li>${obj2[0]}</li>
+          <ul>
+            <li>${obj3[0]}</li>
+            <li>${obj3[1]}</li>
+          </ul>
+        <li>${obj2[1]}</li>
+          <ul>
+            <li>${obj4[0]}</li>
+            <li>${obj4[1]}</li>
+          </ul>
+      </ul>
+  </ul>`;
+  return element;
+
 }
 
 createTree(tree, food);
