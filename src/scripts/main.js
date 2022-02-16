d3260a693b07bae8f20e7a337fe680e4f247c483
Roman Kovalchuk <roma220702@gmail.com>
2022-02-16 17:47:25 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,35 +19,19 @@ const food = {
   },
 };
 const tree = document.querySelector('#tree');
-function createTree(element, data) {
-  const obj = Object.keys(data);
-  const obj1 = Object.keys(data['Drink']);
-  const obj2 = Object.keys(data['Fruit']);
-  const obj3 = Object.keys(data['Fruit']['Red']);
-  const obj4 = Object.keys(data['Fruit']['Yellow']);
-  element.innerHTML = `
-  <ul>
-    <li>${obj[0]}</li>
-      <ul>
-        <li>${obj1[0]}</li>
-        <li>${obj1[1]}</li>
-      </ul>
-    <li>${obj[1]}</li>
-      <ul>
-        <li>${obj2[0]}</li>
-          <ul>
-            <li>${obj3[0]}</li>
-            <li>${obj3[1]}</li>
-          </ul>
-        <li>${obj2[1]}</li>
-          <ul>
-            <li>${obj4[0]}</li>
-            <li>${obj4[1]}</li>
-          </ul>
-      </ul>
-  </ul>`;
-  return element;
 
+function createTree(element, data) {
+  const main = document.createElement('ul');
+  for (let key in data) {
+    const el = document.createElement('li');
+    el.innerText = key;
+    main.append(el);
+    element.append(main);
+    if (Object.keys(data[key]).length > 0) {
+      createTree(main, data[key]);
+    }
+  }
+  return element;
 }
 
 createTree(tree, food);
