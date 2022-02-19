eff01cfae578c4a7a89337760e9c06bd15989b90
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-19 22:31:24 +0200

    The tree traversal script was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,23 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+
+  let nestedList;
+  if (Object.keys(data).length > 0) {
+    console.log('Creating ul')
+    nestedList = document.createElement('ul');
+    element.append(nestedList);
+  }
+
+  for (const elem in data) {
+    if (data[elem]) {
+      const nestedItem = document.createElement('li');
+      nestedItem.textContent = elem;
+      nestedList.append(nestedItem);
+      createTree(nestedItem, data[elem]);
+    }
+  }
 }
 
 createTree(tree, food);
+
