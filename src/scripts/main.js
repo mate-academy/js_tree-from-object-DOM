29be34d8a1bbed0b91971a60303840e5ddba6e43
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-04 13:33:18 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { create } = require("browser-sync");
+
 const food = {
   'Drink': {
     'Wine': {},
@@ -23,6 +25,19 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  for (const obj in data) {
+    const ul = document.createElement('li');
+    const li = document.createElement('ul');
+
+    li.textContent = obj;
+
+    if (data[obj] != {}) {
+      createTree(li, data[obj]);
+    }
+
+    ul.append(li);
+    element.append(ul);
+  }
 }
 
 createTree(tree, food);
