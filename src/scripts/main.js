9a33d1452f7fa05b0ccbce35b9a7df6695550e21
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-08 11:56:16 +0300

    List generator implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,33 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+function generateListFrom(obj) {
+  const list = document.createElement('ul');
+  const items = Object.keys(obj);
+
+  for (const item of items) {
+    const li = document.createElement('li');
+
+    li.innerText = item;
+
+    const subItems = obj[item];
+
+    if (Object.values(subItems).length > 0) {
+      const ul = generateListFrom(subItems);
+
+      li.insertAdjacentElement('beforeend', ul);
+    }
+
+    list.insertAdjacentElement('beforeend', li);
+  }
+
+  return list;
+}
+
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = generateListFrom(data);
+
+  element.insertAdjacentElement('beforeend', list);
 }
 
 createTree(tree, food);
