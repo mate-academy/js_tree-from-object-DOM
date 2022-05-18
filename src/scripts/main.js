30a6b44b964ee9f87efec0f1e3b67721bf6a0c9c
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-05-18 17:19:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// debugger;
+
 const food = {
   'Drink': {
     'Wine': {},
@@ -22,7 +24,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const dataList = Object.keys(data);
+  const newUl = document.createElement('ul');
+
+  for (const product in data) {
+    const liProduct = document.createElement('li');
+
+    liProduct.innerText = product;
+    newUl.append(liProduct);
+
+    if (dataList.length !== 0) {
+      createTree(liProduct, data[product]);
+    }
+  }
+  element.append(newUl);
 }
 
 createTree(tree, food);
