04c2b93769b38f69cdfb70e039ba9db5c6e92623
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-14 21:36:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.appendChild(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.appendChild(li);
+
+    createTree(li, data[key]);
+  }
 }
 
 createTree(tree, food);
