be546b3d5f603d4bd5afbf187ac82265fde90985
Artem Furdela <furdela.artem@gmail.com>
2022-06-16 00:01:49 +0300

    Tree first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,20 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const nestedList = document.createElement('ul');
+
+  for (const key in data) {
+    const item = document.createElement('li');
+
+    item.innerText = key;
+    nestedList.append(item);
+
+    if (Object.values(data[key].length > 0)) {
+      createTree(item, data[key]);
+    }
+
+    element.append(nestedList);
+  }
 }
 
 createTree(tree, food);
