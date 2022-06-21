b8c2430175a63a95c7e47ec992e70057202c6994
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-06-21 17:12:45 +0300

    tree from object DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,19 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  const list = document.createElement('ul');
+
+  for (const value in data) {
+    element.append(list);
+
+    const li = document.createElement('li');
+
+    li.textContent = value;
+    list.append(li);
+
+    createTree(list, data[value]);
+  }
 }
 
 createTree(tree, food);
