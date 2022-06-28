6b7a84593878cf76d9195f3a0edf028d8d42c5bf
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-28 16:45:23 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,22 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  const ul = document.createElement('ul');
+
+  element.appendChild(ul);
+
+  for (const key in data) {
+    const id = Math.random();
+
+    ul.innerHTML += `<li id="${id}">${key}</li>`;
+
+    const li = document.getElementById(`${id}`);
+
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
