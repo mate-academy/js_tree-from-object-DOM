9b80b26d3d4eca731cf1efb6362bac7f9dd58c8d
silchyn <ivan.silchin@gmail.com>
2022-07-05 20:44:00 +0300

    created tree from object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const result = document.createElement('ul');
+
+  for (const [key, value] of Object.entries(data)) {
+    const item = document.createElement('li');
+
+    item.innerText = key;
+
+    if (Object.keys(value).length) {
+      createTree(item, value);
+    }
+
+    result.append(item);
+  }
+
+  element.append(result);
 }
 
 createTree(tree, food);
