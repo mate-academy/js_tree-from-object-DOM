33bbaa6d86d32c4f1a36adfaa0c458551e20508f
Vlad Skuratovskyi <vladskur0369@gmail.com>
2022-07-20 19:06:22 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,31 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  const about = Object.entries(data);
+
+  for (let i = 0; i < about.length; i++) {
+    // if (about[i][1] instanceof Object) {
+    if (Object.keys(about[i][1]).length !== 0) {
+      element.insertAdjacentHTML('beforeend', `
+      <ul>
+        <li class ='last'> ${about[i][0]}
+        </li>
+      </ul>
+      `);
+
+      const base = element.lastElementChild.querySelector('[class ="last"]');
+
+      createTree(base, about[i][1]);
+    } else {
+      element.insertAdjacentHTML('beforeend', `
+    <ul>
+      <li> ${about[i][0]}
+      </li>
+    </ul>
+    `);
+    }
+  }
 }
 
 createTree(tree, food);
