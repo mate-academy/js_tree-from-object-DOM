cf44e24ef7de431ec2078cd9fc3830068bd62a36
Yura Atamaniuk <atam3905@gmail.com>
2022-08-01 16:00:20 +0300

    tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,40 @@
 'use strict';
 
 const food = {
-  'Drink': {
-    'Wine': {},
-    'Schnaps': {},
+  Drink: {
+    Wine: {},
+    Schnaps: {},
   },
 
-  'Fruit': {
-    'Red': {
-      'Cherry': {},
-      'Strawberry': {},
+  Fruit: {
+    Red: {
+      Cherry: {},
+      Strawberry: {},
     },
-    'Yellow': {
-      'Banana': {},
-      'Pineapple': {},
+    Yellow: {
+      Banana: {},
+      Pineapple: {},
     },
-
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  element.append(list);
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.innerText = key;
+    list.append(listItem);
+
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(listItem, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
