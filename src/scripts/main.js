d78232893e1309c2218118d0b6e95d13d59197e3
Bohdan Tron <ikwiczzts@gmail.com>
2022-08-25 15:22:43 +0300

    Tree is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  createStick(data);
+
+  function createStick(newData) {
+    const list = document.createElement('ul');
+
+    for (let i = 0; i < Object.keys(newData).length; i++) {
+      const listItem = document.createElement('li');
+      const nextList = Object.entries(newData[Object.keys(newData)[i]]);
+
+      listItem.textContent = Object.keys(newData)[i];
+      list.append(listItem);
+
+      if (nextList.length > 0) {
+        createTree(list, newData[Object.keys(newData)[i]]);
+      }
+    }
+
+    element.append(list);
+  }
 }
 
 createTree(tree, food);
