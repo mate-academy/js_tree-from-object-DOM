50f9b6b6f30090c8ba8b03530d01c1aa8cf4741c
Sheremet Oleksandra <sandasheremet@gmail.com>
2022-09-19 10:41:15 +0300

    created tree from object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,19 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  for (const iteam in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    li.textContent = iteam;
+    element.append(ul);
+    ul.append(li);
+
+    if (Object.keys(data[iteam]).length !== 0) {
+      createTree(li, data[iteam]);
+    }
+  }
 }
 
 createTree(tree, food);
