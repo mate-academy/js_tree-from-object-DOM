82cb98a3303856387377a7a95831ae01c35c9e2a
grafit-off <graf.off.it@gmail.com>
2022-09-20 20:28:50 +0300

    feat: recursive list was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,7 +15,6 @@ const food = {
       'Banana': {},
       'Pineapple': {},
     },
-
   },
 };
 
@@ -23,6 +22,23 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const internalList = data[key];
+
+    const item = document.createElement('li');
+
+    item.textContent = key;
+
+    if (Object.keys(internalList).length) {
+      createTree(item, internalList);
+    }
+
+    list.append(item);
+  }
+
+  element.append(list);
 }
 
 createTree(tree, food);
