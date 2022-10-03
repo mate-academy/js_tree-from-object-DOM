b0f126ea2dd441b1ff7397640f87c86157df25e6
Serhii Yunnikov <yunnikov@icloud.com>
2022-10-03 07:23:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,19 +24,15 @@ const tree = document.querySelector('#tree');
 function createTree(element, data) {
   const list = document.createElement('ul');
 
-  const createLi = (key) => {
-    list.innerHTML += `
-      <li id = "${key}">
-        ${key}
-      </li>
-    `;
-  };
-
-  Object.keys(data).forEach(createLi);
-
   element.append(list);
 
   for (const item in data) {
+    list.innerHTML += `
+      <li id = "${item}">
+        ${item}
+      </li>
+    `;
+
     if (Object.keys(data[`${item}`]).length !== 0) {
       createTree(document.querySelector(`#${item}`), data[`${item}`]);
     }
