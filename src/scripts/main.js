ff87344da127a70462366f5829ba1c185d2f6ced
Vlad Kurachenko <x0674016244@gmail.com>
2022-10-18 17:28:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,14 +23,16 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   for (const key in data) {
-    element.insertAdjacentHTML('beforeend', `
-      <ul>
-          <li class = "${key}">
-            ${key}
-          </li>
-      </ul>
-    `);
-    createTree(document.getElementsByClassName(key)[0], data[key]);
+    const list = document.createElement('ul');
+    const itemsList = document.createElement('li');
+
+    itemsList.textContent = key;
+    list.append(itemsList);
+    element.append(list);
+
+    if (typeof data[key] === 'object') {
+      createTree(itemsList, data[key]);
+    }
   }
 }
 
