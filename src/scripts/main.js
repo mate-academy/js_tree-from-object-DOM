c618667f7213ee15334950219bfed87ab1798bee
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-12-26 15:15:35 +0200

    Tree from object solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,18 +11,31 @@ const food = {
       'Cherry': {},
       'Strawberry': {},
     },
+
     'Yellow': {
       'Banana': {},
       'Pineapple': {},
     },
-
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    ul.append(li);
+    li.innerHTML = key;
+
+    if (data[key] !== {}) {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
