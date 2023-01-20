e9a54f3f0a5408ed181de86fe4dfe2617e89d7a9
Elena Ver <hellena84@i.ua>
2023-01-20 18:31:10 +0100

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,18 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+
+    li.textContent = key;
+    element.append(ul);
+    ul.append(li);
+
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
