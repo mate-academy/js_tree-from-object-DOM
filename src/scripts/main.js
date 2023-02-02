84c3320021849578ffadbf9ac7a6197a8d56122e
ihorivna007 <emmakorovnik@gmail.com>
2023-02-02 12:52:17 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.insertAdjacentHTML('beforeend', `<ul></ul>`);
+
+  const list = element.lastChild;
+
+  for (const d in data) {
+    list.insertAdjacentHTML('beforeend', `<li>${d}</li>`);
+
+    const listInside = list.lastChild;
+
+    if (Object.keys(data[d]).length !== 0) {
+      createTree(listInside, data[d]);
+    }
+  }
 }
 
 createTree(tree, food);
