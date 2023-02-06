29a6a1c27ccb593aa9bf94e52d92253ac1c10b13
Kate <novak5683@gmail.com>
2023-02-06 20:24:28 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,20 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+function createTree(node, object) {
+  const list = document.createElement('ul');
+
+  for (const child in object) {
+    const item = document.createElement('li');
+
+    item.innerHTML = child;
+
+    if (Object.keys(object[child]).length !== 0) {
+      createTree(item, object[child]);
+    }
+    list.appendChild(item);
+  }
+  node.appendChild(list);
 }
 
 createTree(tree, food);
