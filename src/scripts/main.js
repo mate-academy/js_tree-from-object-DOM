6f7d3b848ccac9ca1d3a1040ce1e533483a5010c
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-09 23:28:20 +0200

    task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,31 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+function createLi(text) {
+  const li = document.createElement('li');
+
+  li.innerHTML = text;
+
+  return li;
+}
+
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const externalList = Object.keys(data);
+  const ul = document.createElement('ul');
+
+  for (let i = 0; i < externalList.length; i++) {
+    const key = externalList[i];
+    const internalList = Object.keys(data[key]);
+    const li = createLi(key);
+
+    ul.append(li);
+
+    if (internalList.length > 0) {
+      createTree(li, data[key]);
+    }
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
