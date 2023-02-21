eac21bb90ee9961107e2aaeaf296327aac6c6764
Max Zelenskyi <rio1391@gmail.com>
2023-02-21 14:58:31 +0100

    Tree from object, Rio

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,30 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  // create an unordered list element
+  const ul = document.createElement('ul');
+
+  // loop through each key in the object
+  for (const key in data) {
+    // create a list item element for the key
+    const li = document.createElement('li');
+
+    li.innerText = key;
+
+    // if the value of the key is an object, call the function recursively
+    if (typeof data[key] === 'object') {
+      li.appendChild(createTree(element, data[key]));
+    }
+
+    // append the list item to the unordered list
+    ul.appendChild(li);
+  }
+
+  // append the unordered list to the given DOM element
+  element.appendChild(ul);
+
+  // return the unordered list
+  return ul;
 }
 
 createTree(tree, food);
