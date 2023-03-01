0346f9d4ce216f740ffba1cf066815d80fcc4b02
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-03-01 18:08:09 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,42 @@
 'use strict';
 
 const food = {
-  'Drink': {
-    'Wine': {},
-    'Schnaps': {},
+  Drink: {
+    Wine: {},
+    Schnaps: {},
   },
 
-  'Fruit': {
-    'Red': {
-      'Cherry': {},
-      'Strawberry': {},
-    },
-    'Yellow': {
-      'Banana': {},
-      'Pineapple': {},
+  Fruit: {
+    Red: {
+      Cherry: {},
+      Strawberry: {},
     },
 
+    Yellow: {
+      Banana: {},
+      Pineapple: {},
+    },
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+  const dataKeys = Object.keys(data);
+
+  dataKeys.forEach((key) => {
+    const li = document.createElement('li');
+    li.innerHTML = key;
+
+    if (typeof data[key] === 'object') {
+      createTree(li, data[key]);
+    }
+    
+    ul.append(li);
+  });
+
+  element.append(ul);
 }
 
 createTree(tree, food);
