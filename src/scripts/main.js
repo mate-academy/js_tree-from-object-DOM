13e04580a5e32301891ae342b4b7b03902a3df12
unknown <vorobetst777@gmail.com>
2023-03-01 19:32:32 +0200

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const newList = document.createElement('ul');
+
+  for (const key in data) {
+    const partOflist = document.createElement('li');
+
+    partOflist.textContent = key;
+
+    if (Object.keys(data[key]).length) {
+      createTree(partOflist, data[key]);
+    }
+
+    newList.append(partOflist);
+  }
+
+  element.append(newList);
 }
 
 createTree(tree, food);
