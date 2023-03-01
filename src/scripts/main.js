8dd3a479ed992084e627fd946d9ca4bac23642e1
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-03-01 18:34:46 +0200

    Returned input data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,19 @@
 'use strict';
 
 const food = {
-  Drink: {
-    Wine: {},
-    Schnaps: {},
+  'Drink': {
+    'Wine': {},
+    'Schnaps': {},
   },
 
-  Fruit: {
-    Red: {
-      Cherry: {},
-      Strawberry: {},
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
     },
-
-    Yellow: {
-      Banana: {},
-      Pineapple: {},
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
     },
   },
 };
@@ -27,12 +26,13 @@ function createTree(element, data) {
 
   dataKeys.forEach((key) => {
     const li = document.createElement('li');
+
     li.innerHTML = key;
 
     if (typeof data[key] === 'object') {
       createTree(li, data[key]);
     }
-    
+
     ul.append(li);
   });
 
