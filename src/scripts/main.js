589e010b05cf225950eb63e28bcee711e808d017
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-03 13:07:18 +0200

    Tree from object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,28 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+function getNestedObjects(data) {
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const item = document.createElement('li');
+
+    item.textContent = key;
+
+    if (JSON.stringify(data[key]) !== '{}') {
+      const listChildren = getNestedObjects(data[key]);
+
+      item.append(listChildren);
+    }
+
+    list.append(item);
+  }
+
+  return list;
+}
+
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.append(getNestedObjects(data));
 }
 
 createTree(tree, food);
