75e71d13741495d8bc334dc4270633fd61715add
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-03-07 12:50:34 +0200

    Tree from object 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,7 +30,7 @@ function createTree(element, data) {
     liElem.textContent = key;
     list.appendChild(liElem);
 
-    if (Object.keys(data[key]).length !== 0) {
+    if (Object.keys(data[key]).length) {
       createTree(liElem, data[key]);
     }
   }
