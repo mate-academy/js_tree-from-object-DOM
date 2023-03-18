f5c9761cdf2848b0ae3e87ebd5774b1041834ad0
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-18 14:29:46 +0100

    create tree list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,16 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const item in data) {
+    const li = document.createElement('li');
+
+    li.textContent = item;
+    ul.append(li);
+    createTree(li, data[item]);
+  }
+  element.append(ul);
 }
 
 createTree(tree, food);
