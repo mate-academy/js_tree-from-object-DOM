f2ffdd55ca28b851d615fce9c2e9efbc0c3f23ab
stetsenko_viktoriia <stetsenko.viktoriia@gmail.com>
2023-03-21 23:04:07 +0200

    tree was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.innerText = key;
+    list.append(listItem);
+
+    element.append(list);
+
+    if (data[key].length !== 0) {
+      createTree(listItem, data[key]);
+    };
+  }
 }
 
 createTree(tree, food);
