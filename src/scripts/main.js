6fae9cc74928b9f509fe349e39df1737522b314a
Maksym Avramenko <nonste@outlook.com>
2023-03-22 17:12:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const text in data) {
+    const item = document.createElement('li');
+
+    item.textContent = text;
+
+    list.append(item);
+
+    createTree(item, data[text]);
+  }
+
+  element.append(list);
 }
 
 createTree(tree, food);
