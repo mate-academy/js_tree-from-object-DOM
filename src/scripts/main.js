18c5fefa4c593afa0f59cc175c95aa2a99c9a135
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-03-28 18:50:05 +0300

    Tree was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,26 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    element.append(ul);
+    ul.append(li);
+
+    li.textContent = key;
+
+    for (const item in createTree(li, data[key])) {
+      const ul2 = document.createElement('ul');
+      const li2 = document.createElement('li');
+
+      li.append(ul2);
+      ul2.append(li2);
+
+      li2.textContent = item;
+    }
+  }
 }
 
 createTree(tree, food);
