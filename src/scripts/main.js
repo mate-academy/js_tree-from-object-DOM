dd16d7d94ed80dea275895ecce980da61aaee5d2
metja <mikolapenzanik@gmail.com>
2023-04-02 10:47:55 +0300

    Completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  Object.keys(data).forEach(key => {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    element.append(ul);
+    ul.append(li);
+    li.textContent = key;
+
+    if (Object.values(key).length > 0) {
+      createTree(li, data[key]);
+    }
+  });
 }
 
 createTree(tree, food);
