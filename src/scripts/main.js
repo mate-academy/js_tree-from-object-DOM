e3781136c024c13d66324de97bb52d09e2511251
Anna Grechana <grechanaanna@gmial.com>
2023-04-05 23:22:50 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const ul = document.createElement('ul');
+
+    element.append(ul);
+
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (typeof data[key] === 'object') {
+      createTree(li, data[key]);
+    }
+  }// WRITE YOUR CODE HERE
 }
 
 createTree(tree, food);
