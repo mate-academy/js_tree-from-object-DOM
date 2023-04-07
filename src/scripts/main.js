ccfe7fe71a82b215e2bdc6c490b70e8548ff555c
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-04-07 11:43:47 +0300

    tree from object was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+    ul.append(li);
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
