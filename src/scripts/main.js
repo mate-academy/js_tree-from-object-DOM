775dc64064b167510fc5dae1eb31176b16adbd4a
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-04-10 12:27:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,44 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const arrOfFood = Object.keys(data);
+  const wrap = document.createElement('ul');
+
+  for (const f of arrOfFood) {
+    const arrSubItems = Object.keys(data[f]);
+    const foodItem = document.createElement('li');
+
+    foodItem.textContent = f;
+    wrap.appendChild(foodItem);
+
+    const subItemWrap = document.createElement('ul');
+
+    foodItem.appendChild(subItemWrap);
+
+    for (const i of arrSubItems) {
+      const subFoodItem = document.createElement('li');
+
+      subFoodItem.textContent = i;
+      subItemWrap.appendChild(subFoodItem);
+
+      if (Object.keys(data[f][i]).length > 0) {
+        const lastArr = Object.keys(data[f][i]);
+
+        for (const item of lastArr) {
+          const lastArrWrap = document.createElement('ul');
+
+          subFoodItem.appendChild(lastArrWrap);
+
+          const lastItem = document.createElement('li');
+
+          lastItem.textContent = item;
+          lastArrWrap.appendChild(lastItem);
+        };
+      }
+    }
+  };
+
+  return tree.appendChild(wrap);
 }
 
 createTree(tree, food);
