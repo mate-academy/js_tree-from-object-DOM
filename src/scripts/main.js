8f8127cffa835aac29ee1c44a5f82413742a1cbf
адмін <admin@Mac-Max.local>
2023-04-17 19:39:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
