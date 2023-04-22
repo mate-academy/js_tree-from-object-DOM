4a1e802801634333a23bef469058034c2b7719cb
danulyk05 <ivan.danulyk2016@gmail.com>
2023-04-22 23:43:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,9 +20,52 @@ const food = {
 };
 
 const tree = document.querySelector('#tree');
+const [drink, fruit] = Object.keys(food);
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  tree.insertAdjacentHTML('afterbegin', `
+    <ul>
+      <li>
+        ${drink}
+        <ul>
+        <li>
+          ${Object.keys(data.Drink)[0]}
+        </li>
+        <li>
+          ${Object.keys(data.Drink)[1]}
+        </li>
+        </ul>
+      </li>
+      <li>
+        ${fruit}
+        <ul>
+          <li>
+            ${Object.keys(data.Fruit)[0]}
+            <ul>
+              <li>
+                ${Object.keys(data.Fruit.Red)[0]}
+              </li>
+              <li>
+              ${Object.keys(data.Fruit.Red)[1]}
+            </li>
+            </ul>
+          </li>
+
+          <li>
+          ${Object.keys(data.Fruit)[1]}
+            <ul>
+              <li>
+                ${Object.keys(data.Fruit.Yellow)[0]}
+              </li>
+              <li>
+              ${Object.keys(data.Fruit.Yellow)[1]}
+              </li>
+            </ul>
+        </li>
+        </ul>
+      </li>
+    </ul>
+  `);
 }
 
 createTree(tree, food);
