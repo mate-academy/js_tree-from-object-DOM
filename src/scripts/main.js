ccea07d69d8fa71c7e0ce14073e3dbd903e132e9
Antonio S <soi4.toxa@ukr.net>
2023-04-27 20:00:57 +0300

    Tree-from-object-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,26 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  let numb = 0;
+  let list;
+  let object;
+
+  for (const key in data) {
+    if (numb === 0) {
+      list = document.createElement('ul');
+
+      element.append(list);
+      numb++;
+    }
+
+    const item = document.createElement('li');
+
+    list.append(item);
+    item.innerText = key;
+    object = data[key];
+
+    createTree(item, object);
+  }
 }
 
 createTree(tree, food);
