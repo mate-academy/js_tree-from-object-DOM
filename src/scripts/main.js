02cad1e4cac161f1ebb092f9ca366adfb6e936ea
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-04-28 19:33:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,46 @@
 'use strict';
 
-const food = {
-  'Drink': {
-    'Wine': {},
-    'Schnaps': {},
-  },
-
-  'Fruit': {
-    'Red': {
-      'Cherry': {},
-      'Strawberry': {},
-    },
-    'Yellow': {
-      'Banana': {},
-      'Pineapple': {},
+document.addEventListener('DOMContentLoaded', function() {
+  const food = {
+    'Drink': {
+      'Wine': {},
+      'Schnaps': {},
     },
 
-  },
-};
+    'Fruit': {
+      'Red': {
+        'Cherry': {},
+        'Strawberry': {},
+      },
+      'Yellow': {
+        'Banana': {},
+        'Pineapple': {},
+      },
 
-const tree = document.querySelector('#tree');
+    },
+  };
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+  const tree = document.querySelector('#tree');
 
-createTree(tree, food);
+  function createTree(element, data) {
+    const ul = document.createElement('ul');
+
+    for (const key in data) {
+      const li = document.createElement('li');
+
+      li.textContent = key;
+
+      if (typeof data[key] === 'object') {
+        li.appendChild(createTree(element, data[key]));
+      }
+
+      ul.appendChild(li);
+    }
+
+    return ul;
+  }
+
+  const root = createTree(tree, food);
+
+  tree.appendChild(root);
+});
