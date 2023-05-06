b78602602ded7a85c3ffbbe617c5abe9ef2dbfc2
Kate Matviiv <katyaya56@gmail.com>
2023-05-06 11:49:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  element.appendChild(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (Object.keys(data[key])) {
+      createTree(li, data[key]);
+    }
+    ul.appendChild(li);
+  };
+
   // WRITE YOUR CODE HERE
 }
 
