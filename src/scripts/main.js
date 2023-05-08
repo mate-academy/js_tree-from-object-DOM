9887635937f2d2e86794e04ba3c3f614697ce46b
Roman <rdyakovich@gmail.com>
2023-05-08 19:09:20 +0300

    object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (Object.keys(data[key]).length) {
+      createTree(li, data[key]);
+    }
+    ul.append(li);
+    element.append(ul);
+  }
 }
 
 createTree(tree, food);
