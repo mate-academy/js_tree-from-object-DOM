3a4ae92f1add8746b2ec80475d2836f8b453751e
metin <metinbicaksiz@gmail.com>
2023-05-09 18:19:04 +0300

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,23 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
+      const nestedUl = createTree(element, data[key]);
+
+      li.appendChild(nestedUl);
+    }
+    ul.appendChild(li);
+  }
+  element.appendChild(ul);
+
+  return ul;
 }
 
 createTree(tree, food);
