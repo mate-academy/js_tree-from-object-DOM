349d651a48186baa9dee120d5ed17c58878cd8fb
Oleksii Lavka <lavka.alexey37gmail.com>
2023-05-10 10:25:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+
+    if (Object.keys(data).length > 0) {
+      list.appendChild(listItem);
+
+      element.appendChild(list);
+    }
+
+    createTree(listItem, data[key]);
+  }
 }
 
 createTree(tree, food);
