4999a4a8f2564cce95d082b9972c577a5807e1c7
Denys Didenko <ddidenko441@gmail.com>
2023-05-15 12:37:31 +0300

    add js_tree-from-object-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const list = document.createElement('ul');
+    const listItem = document.createElement('li');
+
+    element.append(list);
+    list.append(listItem);
+    listItem.innerText = key;
+
+    if (data[key]) {
+      createTree(listItem, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
