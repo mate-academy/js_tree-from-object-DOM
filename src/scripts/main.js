f764ad233250dffd7909f81e843649c8ca7bd864
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-05-18 15:37:28 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,19 +22,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  for (let key in data) {
-      let ul = document.createElement("ul");
-      let li = document.createElement("li");
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
 
-      li.innerText = key;
-      ul.append(li);
-      element.append(ul);
+    li.innerText = key;
+    ul.append(li);
+    element.append(ul);
 
-      if (Object.keys(data[key]).length > 0) {
-        createTree(li, data[key]);
-      }
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
   }
 }
 
 createTree(tree, food);
-
