baf0138d8c811cb4834f3c4dda9f0377749c5121
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2023-05-20 14:19:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (Object.keys(data[key].length > 0)) {
+      const nestedEl = createTree(li, data[key]);
+
+      li.append(nestedEl);
+    }
+
+    ul.append(li);
+  }
+
+  tree.append(ul);
+
+  return ul;
 }
 
 createTree(tree, food);
