a16aab116927da9ea251189a094f77a0280dc1a4
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-05-31 16:12:05 +0300

    'done'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const createUl = document.createElement('ul');
+
+  for (const key in data) {
+    const createLi = document.createElement('li');
+
+    createLi.innerText = key;
+
+    createUl.append(createLi);
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(createLi, data[key]);
+    }
+  }
+
+  element.append(createUl);
 }
 
 createTree(tree, food);
