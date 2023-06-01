9d38cd4a139655c40cbd60197f6d1cc151bcda0b
Roma Tsvindin <tsvindin1995@gmail.com>
2023-06-01 19:23:57 +0300

    js_tree-from-object-DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
+      const nestedUl = createTree(li, data[key]);
+
+      li.appendChild(nestedUl);
+    }
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
+
+  return ul;
 }
 
 createTree(tree, food);
