35502bd211f7ec68b04cefc28f40bd6bdca25a82
abobyr <artem.bobyr@dataart.com>
2023-06-05 12:08:08 +0300

    my privat saves

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,15 +23,15 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   for (const key in data) {
-    const ul = document.createElement('ul')
-    const li = document.createElement('li')
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
 
-    element.append(ul)
-    ul.append(li)
-    li.textContent = key
+    element.append(ul);
+    ul.append(li);
+    li.textContent = key;
 
     if (Object.keys(data[key]).length) {
-      createTree(li, data[key])
+      createTree(li, data[key]);
     }
   }
 }
