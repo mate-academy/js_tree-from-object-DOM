9db492a14edc8ca81ea9809499ce90fea7aa599e
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-06 21:20:40 +0300

    listTree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,17 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    list.append(li);
+    element.append(list);
+
+    createTree(li, data[key]);
+  }
 }
 
 createTree(tree, food);
