411140526c221c36088ee13c6ad738b849b24cd9
Ivan <your@mail.com>
2023-09-21 15:45:05 +0300

    RecursiveTree is ready!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,15 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+  const list = document.createElement('ul');
 
+  for (const item in data) {
+    const listElement = document.createElement('li');
+
+    list.appendChild(listElement);
+    listElement.textContent = item;
+    element.appendChild(list);
+    createTree(listElement, data[item]);
+  }
+}
 createTree(tree, food);
