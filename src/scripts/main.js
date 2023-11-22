89826063788a23e05b2b95dac142f8a53d20a523
Alex Rog <sasharg303@gmail.com>
2023-11-22 14:17:51 +0100

    solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,14 +15,23 @@ const food = {
       'Banana': {},
       'Pineapple': {},
     },
-
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  element.insertAdjacentHTML('beforeend', '<ul></ul>');
+
+  for (const key in data) {
+    element.lastElementChild.insertAdjacentHTML('beforeend', `<li>${key}</li>`);
+
+    const currElem = element.lastElementChild.lastElementChild;
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(currElem, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
