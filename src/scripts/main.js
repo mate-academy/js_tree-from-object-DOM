a25df401015247014366c4366616e536c45469d0
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-08 14:08:15 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 const food = {
@@ -23,6 +24,21 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key of Object.keys(data)) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    ul.append(li);
+
+    if (data[key]) {
+      createTree(li, data[key]);
+    }
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
