4bedf76b4b664d9610d531ccb3379b2864edf632
Lubomur Chuchvara <lubomircucvara@gmail.com>
2024-01-29 19:42:18 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,45 @@
 'use strict';
 
 const food = {
-  'Drink': {
-    'Wine': {},
-    'Schnaps': {},
+  Drink: {
+    Wine: {},
+    Schnaps: {},
   },
 
-  'Fruit': {
-    'Red': {
-      'Cherry': {},
-      'Strawberry': {},
+  Fruit: {
+    Red: {
+      Cherry: {},
+      Strawberry: {},
     },
-    'Yellow': {
-      'Banana': {},
-      'Pineapple': {},
+    Yellow: {
+      Banana: {},
+      Pineapple: {},
     },
-
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listitem = document.createElement('li');
+
+    listitem.textContent = key;
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
+      const nastedList = createTree(document.createElement('ul'), data[key]);
+
+      listitem.appendChild(nastedList);
+    }
+
+    list.appendChild(listitem);
+  }
+
+  element.appendChild(list);
+
+  return list;
 }
 
 createTree(tree, food);
