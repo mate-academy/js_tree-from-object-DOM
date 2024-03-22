b8466f2820810a472610ed04b3c24942e2ce45a7
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-03-22 12:06:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,23 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  element.append(list);
+
+  for (const val of Object.entries(data)) {
+    const item = document.createElement('li');
+
+    item.innerText = val[0];
+    list.append(item);
+    createTree(item, val[1]);
+
+    // if (val.length > 0) {
+    //   item.innerText = val[0];
+    //   list.append(item);
+    //   createTree(item, val[1]);
+    // }
+  }
 }
 
 createTree(tree, food);
