441facb154d3d8de2b68a7934bfd3beea2bcf932
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-27 13:00:02 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,26 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const root = document.createElement('ul');
+
+  for (const info in data) {
+    const branch = document.createElement('li');
+
+    branch.textContent = info;
+
+    if (Object.keys(data[info]).length > 0) {
+      createTree(branch, data[info]);
+    }
+
+    root.append(branch);
+  }
+
+  if (element) {
+    element.append(root);
+  } else {
+    root.id = 'tree';
+    document.body.prepend(root);
+  }
 }
 
 createTree(tree, food);
