ee59c438153b46a78862cef1c1afee59adedd557
Vladyslav <vladbuldenko@gmail.com>
2024-06-08 17:01:12 +0300

    VB solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const [key, value] of Object.entries(data)) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (Object.keys(value).length > 0) {
+      createTree(li, value);
+    }
+  }
 }
 
 createTree(tree, food);
