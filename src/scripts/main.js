05b647e0d94530ab476f75c6e2736297ff75dd5d
Den <clixqqk@gmail.com>
2024-06-09 14:39:15 +0300

    dev

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,29 @@ const food = {
   },
 };
 
+const list = document.createElement('ul');
+
+list.setAttribute('id', 'tree');
+document.body.append(list);
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const liElement = document.createElement('li');
+
+    liElement.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      const ulElement = document.createElement('ul');
+
+      liElement.append(ulElement);
+
+      createTree(ulElement, data[key]);
+    }
+
+    element.append(liElement);
+  }
 }
 
 createTree(tree, food);
