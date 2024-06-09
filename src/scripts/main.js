c52d60fab4cbf83f9b574bebaf293b5a4577a8fe
deezhizyu <kalenskuja@gmail.com>
2024-06-09 22:27:35 +0200

    Implement task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,30 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  let targetList = element;
+
+  if (!targetList) {
+    targetList = document.createElement('ul');
+    targetList.setAttribute('id', 'tree');
+
+    document.body.append(targetList);
+  }
+
+  for (const [key, value] of Object.entries(data)) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+
+    if (Object.values(value).length > 0) {
+      const nestedList = document.createElement('ul');
+
+      listItem.append(nestedList);
+
+      createTree(nestedList, value);
+    }
+
+    targetList.append(listItem);
+  }
 }
 
 createTree(tree, food);
