5b1007345384c5833baacf212c78e4389b689a30
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-10 15:31:17 +0300

    Created tree builder.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const foodType in data) {
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+
+    li.textContent = foodType;
+
+    createTree(ul, data[foodType]);
+
+    if (ul.hasChildNodes()) {
+      li.append(ul);
+    }
+
+    element.append(li);
+  }
 }
 
 createTree(tree, food);
