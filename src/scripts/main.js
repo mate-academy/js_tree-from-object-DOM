f58373da0a67d3776dc718936b0cd01babb5dc18
Yurii <urabelivr@gmail.com>
2024-06-13 14:45:11 +0200

    added list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,22 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const [key, value] of Object.entries(data)) {
+    const listItem = document.createElement('li');
+
+    listItem.append(key);
+
+    if (Object.keys(value).length === 0) {
+      element.appendChild(listItem);
+    } else {
+      const unorderedList = document.createElement('ul');
+
+      createTree(unorderedList, value);
+
+      listItem.appendChild(unorderedList);
+      element.appendChild(listItem);
+    }
+  }
 }
 
 createTree(tree, food);
