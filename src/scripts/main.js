ecea0e85a91fb9a5458646474715c90ab19f9eb0
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-15 16:35:25 +0300

    The tree from the object has been grown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  if (!element) {
+    ul.id = 'tree';
+    document.body.prepend(ul);
+  } else {
+    element.append(ul);
+  }
+
+  Object.entries(data).forEach(([key, value]) => {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (Object.keys(value).length > 0) {
+      createTree(li, value);
+    }
+  });
 }
 
 createTree(tree, food);
