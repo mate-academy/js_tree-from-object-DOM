3fabc121526ab2ea1c5e61be3ea8a4c2e4d0edec
Victor Skits <vityok.nice@gmail.com>
2024-06-17 12:21:59 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const itemType in data) {
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+
+    li.textContent = itemType;
+
+    createTree(ul, data[itemType]);
+
+    if (ul.hasChildNodes()) {
+      li.append(ul);
+    }
+
+    element.append(li);
+  }
 }
 
 createTree(tree, food);
