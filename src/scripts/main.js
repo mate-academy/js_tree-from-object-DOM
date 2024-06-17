577a4fe9a5fc9c87f380314a42b41e9b3bc71ce4
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-17 15:26:45 +0300

    [Feature] added tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,8 +20,32 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+function makeTree(data) {
+  const rootList = document.createElement('ul');
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+
+    const hasNestedList = !!Object.keys(data[key]).length;
+
+    if (hasNestedList) {
+      const list = makeTree(data[key]);
+
+      listItem.append(list);
+    }
+
+    rootList.append(listItem);
+  }
+
+  return rootList;
+}
+
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const listTree = makeTree(data);
+
+  element.prepend(listTree);
 }
 
 createTree(tree, food);
