d5dad86c4bdcc98b44f912ac699912e43daa54a4
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-17 10:51:14 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const keys = Object.keys(data);
+
+  if (keys.length === 0) {
+    return;
+  }
+
+  const list = document.createElement('ul');
+
+  for (const key of keys) {
+    const item = document.createElement('li');
+
+    item.insertAdjacentText('beforeend', key);
+
+    createTree(item, data[key]);
+
+    list.insertAdjacentElement('beforeend', item);
+  }
+
+  element.insertAdjacentElement('beforeend', list);
 }
 
 createTree(tree, food);
