dcfc43e7fbd8d485d8a62e6f8805932124f45399
Marian <getmoodyup@gmail.com>
2024-06-18 16:17:31 +0300

    createTree func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const li = document.createElement('li');
+
+      li.textContent = key;
+
+      const nestedData = data[key];
+
+      if (Object.keys(nestedData).length > 0) {
+        createTree(li, nestedData);
+      }
+
+      ul.appendChild(li);
+    }
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
