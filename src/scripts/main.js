f280f61aa56334ccb37ffe3453388c4cd40b62ce
Nazarii <Kyzenko.nr@gmail.com>
2024-06-19 22:11:18 +0300

    FixedAddUlBeforeFunction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,20 +22,21 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
   for (const key in data) {
-    const ul = document.createElement('ul');
     const li = document.createElement('li');
 
     li.textContent = key;
 
-    ul.append(li);
-    element.append(ul);
-
     if (Object.keys(data[key]).length) {
       createTree(li, data[key]);
     }
-    
+
+    ul.append(li);
   }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
