9fc08f9323d24a140bb43e3b6d2cc028a64d59a9
Nazarii <Kyzenko.nr@gmail.com>
2024-06-19 21:45:36 +0300

    DevelopCommitOne

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,20 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    ul.append(li);
+    element.append(ul);
+
+    if (Object.keys(data[key]).length) {
+      createTree(li, data[key]);
+    }
+    
+  }
 }
 
 createTree(tree, food);
