3311513ceded81bac2bb24e80736c5ef41c997a0
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-22 17:23:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,11 +17,20 @@ const food = {
     },
   },
 };
-
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+  for (const key in data) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
 
+    if (typeof data[key] === 'object' && data[key] !== null) {
+      element.appendChild(ul);
+      li.textContent = key;
+      ul.appendChild(li);
+
+      createTree(ul, data[key]);
+    }
+  }
+}
 createTree(tree, food);
