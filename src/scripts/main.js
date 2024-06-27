fe7ba316ce4447128bf8ce15a793bdd62a751d8a
luter <newgglutt@gmail.com>
2024-06-27 18:47:01 +0300

    new

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    createTree(li, data[key]);
+
+    ul.appendChild(li);
+  }
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
