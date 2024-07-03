02276808e7666a6a2480ec0de0f8dca87c02dfa0
valentyn19939 <valik.hai1993@gmail.com>
2024-07-03 18:22:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,31 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const treeElement = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  function createListFromObject(obj) {
+    const ul = document.createElement('ul');
+
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const li = document.createElement('li');
+
+        li.textContent = key;
+
+        if (typeof obj[key] === 'object' && Object.keys(obj[key]).length > 0) {
+          li.appendChild(createListFromObject(obj[key]));
+        }
+        ul.appendChild(li);
+      }
+    }
+
+    return ul;
+  }
+
+  const tree = createListFromObject(data);
+
+  element.appendChild(tree);
 }
 
-createTree(tree, food);
+createTree(treeElement, food);
