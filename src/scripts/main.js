aec4456eb2943d2332075e1e3748cbeb55d1448c
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-03 15:39:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,8 +20,38 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+const isEmptyObj = (obj) => {
+  return Object.values(obj).every((value) => Object.keys(value).length === 0);
+};
+
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (isEmptyObj(data)) {
+    const nextUlElement = document.createElement('ul');
+
+    Object.keys(data).forEach((item) => {
+      const liItem = document.createElement('li');
+
+      liItem.textContent = item;
+      nextUlElement.append(liItem);
+    });
+
+    element.append(nextUlElement);
+
+    return;
+  }
+
+  const ulElement = document.createElement('ul');
+
+  Object.entries(data).map(([key, value]) => {
+    const liItem = document.createElement('li');
+
+    liItem.textContent = key;
+    ulElement.append(liItem);
+
+    createTree(liItem, value);
+  });
+
+  element.append(ulElement);
 }
 
 createTree(tree, food);
