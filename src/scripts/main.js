e2e71078f3a2fcb832566098496441778626d11d
valost <valerieostrovska@yahoo.com>
2024-07-13 19:49:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  Object.keys(data).forEach((key) => {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+  });
 }
 
 createTree(tree, food);
