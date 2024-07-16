8f18fd1e7404e42ccc9d05463a563c98dad0cef2
Ivan <tixehack@gmail.com>
2024-07-16 11:45:25 +0300

    feat: add logic to complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const newul = document.createElement('ul');
+
+  for (const key in data) {
+    const newli = document.createElement('li');
+
+    newli.textContent = key;
+
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(newli, data[key]);
+    }
+
+    newul.append(newli);
+  }
+
+  element.append(newul);
 }
 
 createTree(tree, food);
