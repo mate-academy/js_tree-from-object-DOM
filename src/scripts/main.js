c9cc385a9f61564c34712ece023770143a36e633
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-21 14:23:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const li = document.createElement('li');
+
+      li.textContent = key;
+
+      if (typeof data[key] === 'object' && data[key] !== null) {
+        createTree(li, data[key]);
+      } else {
+        li.textContent += `: ${data[key]}`;
+      }
+
+      ul.appendChild(li);
+    }
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
