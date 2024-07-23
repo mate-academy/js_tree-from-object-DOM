f3df46f8fb0fe86b999b356c090d5f463c9c9ae5
Your Name <ruslankurman13@gmail.com>
2024-07-23 16:59:52 +0300

    made a link

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,3 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
-  rules: {
-    'linebreak-style': ['error', 'unix'], // використовуйте 'unix' для LF або 'windows' для CRLF
-  },
 };
