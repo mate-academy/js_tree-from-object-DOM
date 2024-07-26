39bf3f8c39bce3c97688325208dc54fa1e21749c
Denys209818 <deniskravcuk63@gmail.com>
2024-07-27 02:16:17 +0300

    Load solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,23 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  for (const key in data) {
+    const isEmpty = Object.keys(data[key]).length === 0;
+    const liItem = document.createElement('li');
+
+    liItem.innerHTML = key;
+
+    if (!isEmpty) {
+      const liList = document.createElement('ul');
+
+      liItem.append(liList);
+
+      createTree(liList, data[key]);
+    }
+
+    element.append(liItem);
+  }
 }
 
 createTree(tree, food);
