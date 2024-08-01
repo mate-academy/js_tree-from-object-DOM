3aa60c755cdb60a1a89dd7dd094c639268ef2b6d
MaksymKolesnyk <maksymka@icloud.com>
2024-07-31 22:59:03 -0400

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,17 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const el in data) {
+    const li = document.createElement('li');
+    li.textContent = el;
+    element.append(li);
+
+    if(Object.keys(data[el]).length > 0) {
+      const ul = document.createElement('ul');
+      element.append(ul);
+      createTree(ul, data[el]);
+    }
+  }
 }
 
 createTree(tree, food);
