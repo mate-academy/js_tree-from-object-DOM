f688ecbba8cd9ef297700a0af7082d6c9587b912
MaksymKolesnyk <maksymka@icloud.com>
2024-08-02 00:00:09 -0400

    fixing errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,17 +21,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
+  const unList = document.createElement('ul');
+
   for (const el in data) {
     const li = document.createElement('li');
-    li.textContent = el;
-    element.append(li);
 
-    if(Object.keys(data[el]).length > 0) {
-      const ul = document.createElement('ul');
-      element.append(ul);
-      createTree(ul, data[el]);
+    li.textContent = el;
+    unList.append(li);
+
+    if (Object.keys(data[el]).length > 0) {
+      createTree(li, data[el]);
     }
   }
+
+  element.append(unList);
 }
 
 createTree(tree, food);
