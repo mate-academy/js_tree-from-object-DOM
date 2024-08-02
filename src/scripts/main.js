3f85afb8d202e62b07739b7af93c61ad3885618a
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-02 19:35:43 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,27 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.appendChild(ul);
+
+  for (const item of Object.entries(data)) {
+    const li = document.createElement('li');
+
+    li.textContent = item[0];
+    ul.appendChild(li);
+
+    if (
+      item[1] &&
+      typeof item[1] === 'object' &&
+      Object.keys(item[1]).length > 0
+    ) {
+      const nextUl = document.createElement('ul');
+
+      element.appendChild(nextUl);
+      createTree(li, item[1]);
+    }
+  }
 }
 
 createTree(tree, food);
