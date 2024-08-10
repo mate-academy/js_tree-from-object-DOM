0d885e49de36b5136f283383085bed6a574b311d
Mainaiev <farid.mainaiev@proton.me>
2024-08-10 04:58:55 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,35 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+// const tree = document.querySelector('#tree');
+
+const tree = createDomElement();
+
+createTree(tree, food);
+
+function createDomElement() {
+  const body = document.querySelector('body');
+  const domTree = document.createElement('ul');
+
+  domTree.id = 'tree';
+  body.append(domTree);
+
+  return domTree;
+}
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
-}
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const li = document.createElement('li');
 
-createTree(tree, food);
+      li.innerText = key;
+      element.append(li);
+
+      const ul = document.createElement('ul');
+
+      li.append(ul);
+      createTree(ul, data[key]);
+    }
+  }
+}
