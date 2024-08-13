2be1373fa65f980f7a7e5f5be900528699d4a5ae
Ivan <fanmetalheavy@gmail.com>
2024-08-13 18:15:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,25 @@ const food = {
   },
 };
 
+document.body.setAttribute('id', 'tree');
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    list.append(li);
+
+    if (Object.values(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+  }
+
+  element.append(list);
 }
 
 createTree(tree, food);
