43bac450721420e9cab0c242a59187eaa4757eee
Andriy <andreyysak17@gmail.com>
2024-08-14 10:34:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,28 @@ const food = {
   },
 };
 
+document.body.setAttribute('id', 'tree');
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!data) {
+    return;
+  }
+
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+
+    createTree(listItem, data[key]);
+
+    list.appendChild(listItem);
+  }
+
+  element.appendChild(list);
 }
 
 createTree(tree, food);
