92a4fd03989b3a62cde30fd86f7a03572a95a71f
Valentin_Kuianov <kuyanov1988@mail.com>
2024-08-19 19:08:10 +0300

    Second

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,23 +22,20 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
-  if (Object.keys(data).length === 0) {
-    return;
-  }
+  const list = document.createElement('ul');
 
-  const ul = document.createElement('ul');
-
-  element.appendChild(ul);
+  element.append(list);
 
   for (const key in data) {
-    if (data.hasOwnProperty(key)) {
-      const li = document.createElement('li');
+    const listElement = document.createElement('li');
 
-      li.textContent = key;
-      ul.appendChild(li);
+    listElement.textContent = key;
 
-      createTree(li, data[key]);
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(listElement, data[key]);
     }
+
+    list.append(listElement);
   }
 }
 
