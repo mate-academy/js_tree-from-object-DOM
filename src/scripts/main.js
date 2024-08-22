2e43e576f89a5384575ebb03b4813539e7d1f282
Serhii Babskyi <sergbald99@gmail.com>
2024-08-22 22:29:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,43 @@ const food = {
   },
 };
 
+document.body.id = 'tree';
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const fragment = document.createDocumentFragment();
+
+  function buildTree(obj) {
+    if (!Object.keys(obj).length) {
+      return;
+    }
+
+    const ul = document.createElement('ul');
+
+    for (const key in obj) {
+      const li = document.createElement('li');
+
+      li.textContent = key;
+
+      const nestedUl = buildTree(obj[key]);
+
+      if (nestedUl) {
+        li.append(nestedUl);
+      }
+
+      ul.append(li);
+    }
+
+    return ul;
+  }
+
+  fragment.append(buildTree(data));
+  element.append(fragment);
 }
 
 createTree(tree, food);
