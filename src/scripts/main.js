9bfe5a75a4bdbe6e8da52a1240ba27dd63ee7b6c
sinner1993 <sergio1993goga@gmail.com>
2024-08-25 16:21:21 +0200

    1234

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,34 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul1Lvl = document.createElement('ul');
+
+  element.append(ul1Lvl);
+
+  for (const key1 in data) {
+    const li1Lvl = document.createElement('li');
+    const ul2Lvl = document.createElement('ul');
+
+    li1Lvl.textContent = key1;
+    ul1Lvl.append(li1Lvl);
+    ul1Lvl.append(ul2Lvl);
+
+    for (const key2 in data[key1]) {
+      const li2Lvl = document.createElement('li');
+      const ul3Lvl = document.createElement('ul');
+
+      li2Lvl.textContent = key2;
+      ul2Lvl.append(li2Lvl);
+      ul2Lvl.append(ul3Lvl);
+
+      for (const key3 in data[key1][key2]) {
+        const li3Lvl = document.createElement('li');
+
+        li3Lvl.textContent = key3;
+        ul3Lvl.append(li3Lvl);
+      }
+    }
+  }
 }
 
 createTree(tree, food);
