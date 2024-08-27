563e07a57b7675b8b1fc22814e9817a4784a2c74
sinner1993 <sergio1993goga@gmail.com>
2024-08-27 22:08:08 +0200

    adsasdasd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,35 +21,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  const ul1Lvl = document.createElement('ul');
+  if (typeof data !== 'object') {
+    return;
+  }
 
-  element.append(ul1Lvl);
+  const ul = document.createElement('ul');
 
-  for (const key1 in data) {
-    const li1Lvl = document.createElement('li');
-    const ul2Lvl = document.createElement('ul');
+  for (const key in data) {
+    const li = document.createElement('li');
 
-    li1Lvl.textContent = key1;
-    ul1Lvl.append(li1Lvl);
-    li1Lvl.append(ul2Lvl);
+    li.textContent = key;
 
-    for (const key2 in data[key1]) {
-      const li2Lvl = document.createElement('li');
-      const ul3Lvl = document.createElement('ul');
-
-      ul2Lvl.append(li2Lvl);
-      li2Lvl.textContent = key2;
-
-      if (li2Lvl.textContent !== 'Wine' && li2Lvl.textContent !== 'Schnaps') {
-        li2Lvl.append(ul3Lvl);
-      }
-
-      for (const key3 in data[key1][key2]) {
-        const li3Lvl = document.createElement('li');
-
-        li3Lvl.textContent = key3;
-        ul3Lvl.append(li3Lvl);
-      }
+    if (typeof data[key] === 'object') {
+      createTree(li, data[key]);
+      ul.append(li);
+      element.append(ul);
     }
   }
 }
