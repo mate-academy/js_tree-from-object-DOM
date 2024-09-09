3c0c613090328d1684a59138ffc398fb35c6a2c6
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-10 00:11:30 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,28 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  function createList(intel) {
+    const list = document.createElement('ul');
+
+    for (const key in intel) {
+      const listItem = document.createElement('li');
+
+      listItem.textContent = key;
+
+      if (
+        typeof intel[key] === 'object' &&
+        Object.keys(intel[key]).length > 0
+      ) {
+        listItem.appendChild(createList(intel[key]));
+      }
+
+      list.appendChild(listItem);
+    }
+
+    return list;
+  }
+
+  element.appendChild(createList(data));
 }
 
 createTree(tree, food);
