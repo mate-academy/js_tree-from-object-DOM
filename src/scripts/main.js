112a9e0bfea5bd6f766a9f9b351f0032ca385643
Mykhailo Lapchynskyi <mykhailolapchynskyi@mac.local>
2024-09-29 14:40:44 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,24 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    if (!element) {
+      return;
+    }
+
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+    element.append(listItem);
+
+    if (Object.keys(data[key]).length !== 0 && typeof data[key] === 'object') {
+      const list = document.createElement('ul');
+
+      listItem.append(list);
+
+      createTree(list, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
