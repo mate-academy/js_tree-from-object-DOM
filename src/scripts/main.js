84893746a64711965b35bae83a111535a28995db
Valentyna <adm@adms-MacBook-Pro.local>
2025-01-07 20:27:31 +0200

    add tree from object dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!element || !data) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const li = document.createElement('li');
+
+      li.textContent = key;
+      ul.appendChild(li);
+
+      if (Object.keys(data[key]).length > 0) {
+        createTree(li, data[key]);
+      }
+    }
+  }
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
