f181480b0727e40a45385ff59f9af555b73b3538
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-01-20 16:47:40 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (typeof data[key] === 'object' && data[key] !== null) {
+      createTree(li, data[key]);
+    } else {
+      const value = document.createElement('span');
+
+      value.textContent = `${data[key]}`;
+      li.appendChild(value);
+    }
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
