3a2944ef007e4921714f7fcd2aa6b830709156fe
Vitalii Kredinets <kredinec.v@gmail.com>
2025-02-03 16:14:00 +0200

    solve task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,34 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+// const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  if (Object.keys(data).length === 0) {
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    createTree(li, data[key]);
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
 
-createTree(tree, food);
+const treeContainer = document.createElement('div');
+
+treeContainer.id = 'tree';
+
+document.body.appendChild(treeContainer);
+
+createTree(treeContainer, food);
