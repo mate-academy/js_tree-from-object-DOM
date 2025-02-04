76208ae20a61f4168d798d7207f124c0da625d74
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-04 17:59:10 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const food = {
+const food = { // data
   Drink: {
     Wine: {},
     Schnaps: {},
@@ -18,10 +18,28 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+// const tree = document.querySelector('#tree'); // element
+const tree = document.createElement('div');
+
+document.body.append(tree);
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  element.append(list);
+
+  Object.keys(data).forEach((elem) => {
+    const item = document.createElement('li');
+
+    item.textContent = elem;
+    list.append(item);
+
+    const subdata = data[elem];
+
+    if (typeof subdata === 'object' && Object.keys(subdata).length > 0) {
+      createTree(item, subdata);
+    }
+  });
 }
 
 createTree(tree, food);
