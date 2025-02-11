281313e1b00e9e6f6b23fd48920daee8bf195265
Egor Mamtsev <egormamcev@MacBook-Air.local>
2025-02-11 12:18:14 -0600

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,33 @@
 'use strict';
 
 const food = {
-  Drink: {
-    Wine: {},
-    Schnaps: {},
-  },
+  Drink: { Wine: {}, Schnaps: {} },
 
   Fruit: {
-    Red: {
-      Cherry: {},
-      Strawberry: {},
-    },
-    Yellow: {
-      Banana: {},
-      Pineapple: {},
-    },
+    Red: { Cherry: {}, Strawberry: {} },
+    Yellow: { Banana: {}, Pineapple: {} },
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (Object.keys(data).length === 0) {
+    return;
+  }
+
+  for (const subitem in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = subitem;
+
+    const subList = document.createElement('ul');
+
+    createTree(subList, data[subitem]);
+
+    listItem.append(subList);
+    element.append(listItem);
+  }
 }
 
 createTree(tree, food);
