98fd164f5ce54daebc730add5df239e635829f02
David Moskalenko <dvdmsk21@gmail.com>
2025-02-15 21:33:28 +0200

    recursion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable padding-line-between-statements */
 'use strict';
 
 const food = {
@@ -18,10 +19,26 @@ const food = {
   },
 };
 
+const treeList = document.createElement('div');
+treeList.id = 'tree';
+document.body.appendChild(treeList);
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  if (Object.keys(data).length === 0) {
+    return undefined;
+  } else {
+    const ul = document.createElement('ul');
+    element.appendChild(ul);
+    for (const key of Object.keys(data)) {
+      const curLi = document.createElement('li');
+      ul.appendChild(curLi);
+      curLi.textContent = key;
+      createTree(curLi, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
