755b13e2d7946342903db9fbb2f8777bcbeeac99
Vitalii Kornyliuk <vitalii.kornyliuk@aspose.com>
2025-02-20 16:47:51 +0300

    list added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  element.appendChild(list);
+
+  Object.keys(data).forEach((obj) => {
+    const li = document.createElement('li');
+
+    li.textContent = obj;
+    list.appendChild(li);
+
+    if (Object.keys(data[obj]).length > 0) {
+      createTree(li, data[obj]);
+    }
+  });
 }
 
 createTree(tree, food);
