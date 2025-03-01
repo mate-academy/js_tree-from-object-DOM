6c7ccda2a966fe6282e3c833e1a7611b96b27343
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-03-01 18:51:13 +0100

    treeFromObject

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,33 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  function createTree(element, data) {
+    function createList(data) {
+      const ul = document.createElement('ul');
+  
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          const li = document.createElement('li');
+          
+          li.textContent = key;
+  
+          if (typeof data[key] === 'object' && data[key] !== null) {
+            li.appendChild(createList(data[key]));
+          } else {
+            li.textContent = `${key}: ${data[key]}`;
+          }
+ 
+          ul.appendChild(li);
+        }
+      }
+  
+      return ul; // Return the constructed ul
+    }
+  
+    element.innerHTML = '';
+    element.appendChild(createList(data));
+  }
+  
 }
 
 createTree(tree, food);
