70caa9ca15a606e3ed320fd445e7b222fb43bd48
Maciej <metro1568@gmail.com>
2025-03-24 15:45:51 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,45 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const tree = document.createElement('ul');
+const scheme = document.createElement('ul');
+let firstIteration = true;
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+function generateHTMLList(sourceJSObject, reproducedHTML = scheme) {
+  const isEmpty = Object.keys(sourceJSObject).length === 0;
+  const isObject = typeof sourceJSObject === 'object';
+  let pathToInsideUL = reproducedHTML;
+
+  if (!isObject || isEmpty) {
+    return null;
+  } else {
+    if (firstIteration) {
+      firstIteration = false;
+    } else {
+      reproducedHTML.appendChild(document.createElement('ul'));
+      pathToInsideUL = reproducedHTML.firstElementChild;
+    }
+
+    Object.keys(sourceJSObject).forEach((key) => {
+      pathToInsideUL.appendChild(document.createElement('li'));
+      pathToInsideUL.lastElementChild.textContent = key;
+      generateHTMLList(sourceJSObject[key], pathToInsideUL.lastElementChild);
+    });
+  }
+
+  return scheme;
 }
 
-createTree(tree, food);
+const list = generateHTMLList(food);
+
+function createTree(element, data) {
+  element.appendChild(data);
+
+  const whatToAdd = element.querySelector('ul');
+
+  whatToAdd.setAttribute('id', 'tree');
+
+  document.body.appendChild(whatToAdd);
+}
+
+createTree(tree, list);
