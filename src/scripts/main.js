ae77feb273d1c3759dfcc3fd11ce9f5e9017a7f8
Maciej <metro1568@gmail.com>
2025-03-31 13:40:10 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,45 +18,43 @@ const food = {
   },
 };
 
-const tree = document.createElement('ul');
-const scheme = document.createElement('ul');
-let firstIteration = true;
+const tree = document.createElement('div');
 
-function generateHTMLList(sourceJSObject, reproducedHTML = scheme) {
-  const isEmpty = Object.keys(sourceJSObject).length === 0;
-  const isObject = typeof sourceJSObject === 'object';
-  let pathToInsideUL = reproducedHTML;
-
-  if (!isObject || isEmpty) {
-    return null;
-  } else {
-    if (firstIteration) {
-      firstIteration = false;
-    } else {
-      reproducedHTML.appendChild(document.createElement('ul'));
-      pathToInsideUL = reproducedHTML.firstElementChild;
-    }
-
-    Object.keys(sourceJSObject).forEach((key) => {
-      pathToInsideUL.appendChild(document.createElement('li'));
-      pathToInsideUL.lastElementChild.textContent = key;
-      generateHTMLList(sourceJSObject[key], pathToInsideUL.lastElementChild);
-    });
-  }
-
-  return scheme;
-}
-
-const list = generateHTMLList(food);
+tree.setAttribute('id', 'tree');
+document.body.appendChild(tree);
 
 function createTree(element, data) {
-  element.appendChild(data);
+  const scheme = document.createElement('ul');
+  let firstIteration = true;
 
-  const whatToAdd = element.querySelector('ul');
+  const generateHTMLList = function (sourceJSObject, reproducedHTML = scheme) {
+    const isEmpty = Object.keys(sourceJSObject).length === 0;
+    const isObject = typeof sourceJSObject === 'object';
+    let pathToInsideUL = reproducedHTML;
 
-  whatToAdd.setAttribute('id', 'tree');
+    if (!isObject || isEmpty) {
+      return null;
+    } else {
+      if (firstIteration) {
+        firstIteration = false;
+      } else {
+        reproducedHTML.appendChild(document.createElement('ul'));
+        pathToInsideUL = reproducedHTML.firstElementChild;
+      }
 
-  document.body.appendChild(whatToAdd);
+      Object.keys(sourceJSObject).forEach((key) => {
+        pathToInsideUL.appendChild(document.createElement('li'));
+        pathToInsideUL.lastElementChild.textContent = key;
+        generateHTMLList(sourceJSObject[key], pathToInsideUL.lastElementChild);
+      });
+    }
+
+    return scheme;
+  };
+
+  const list = generateHTMLList(data);
+
+  element.appendChild(list);
 }
 
-createTree(tree, list);
+createTree(tree, food);
