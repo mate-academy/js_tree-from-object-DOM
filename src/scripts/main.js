71c830cfb9363c776ab6f99b1add94e758929625
apr1ori <vladgolenok4@gmail.com>
2025-04-09 12:41:48 +0200

    task 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,34 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
-
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      const children = createTree(null, data[key]);
+
+      li.appendChild(children);
+    }
+
+    ul.appendChild(li);
+  }
+
+  if (element) {
+    element.appendChild(ul);
+
+    return;
+  }
+
+  return ul;
 }
 
+const tree = document.createElement('ul');
+
+tree.id = 'tree';
+document.body.appendChild(tree);
 createTree(tree, food);
