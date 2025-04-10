0f36c4e801ef0b03c1410b9a162d449087846529
AnoriFrell <anori.fjord@gmail.com>
2025-04-10 20:31:24 +0300

    tree complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,28 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const body = document.querySelector('body');
+const tree = document.createElement('div');
+
+tree.setAttribute('id', 'tree');
+
+body.append(tree);
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const [key, value] of Object.entries(data)) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+    list.appendChild(listItem);
+
+    if (Object.keys(value).length > 0) {
+      createTree(listItem, value);
+    }
+  }
+  element.appendChild(list);
 }
 
 createTree(tree, food);
