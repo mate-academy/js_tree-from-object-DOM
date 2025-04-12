dd02604e597d440e2dc81771b9347b726d87f36f
Vhatever <fserrorka@gmail.com>
2025-04-12 13:03:34 +0300

    tryN1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,41 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+
+    span.textContent = key;
+
+    li.appendChild(span);
+
+    if (Object.keys(data[key]).length > 0) {
+      const childUl = document.createElement('ul');
+
+      for (const childKey in data[key]) {
+        const childLi = document.createElement('li');
+        const childSpan = document.createElement('span');
+
+        childSpan.textContent = childKey;
+
+        childLi.appendChild(childSpan);
+
+        if (Object.keys(data[key][childKey]).length > 0) {
+          createTree(childLi, data[key][childKey]);
+        }
+
+        childUl.appendChild(childLi);
+      }
+
+      li.appendChild(childUl);
+    }
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
