69ad8ee9cc4d9f62075645037b621fc4d2383cda
serhiy23471 <sergsy.pyatachenko@gmail.com>
2025-04-18 09:19:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  // Якщо data порожній, нічого не робимо
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    // Рекурсивно додаємо піддерево, якщо воно є
+    createTree(li, data[key]);
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
