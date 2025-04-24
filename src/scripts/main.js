76dbeb50db7e0974c896eafd07551322cc66bad5
g.palyukh <g.palyukh@gmail.com>
2025-04-24 19:39:41 +0200

    create nested list from object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,31 @@
 'use strict';
 
 const food = {
-  Drink: {
-    Wine: {},
-    Schnaps: {},
-  },
+  Drink: { Wine: {}, Schnaps: {} },
 
   Fruit: {
-    Red: {
-      Cherry: {},
-      Strawberry: {},
-    },
-    Yellow: {
-      Banana: {},
-      Pineapple: {},
-    },
+    Red: { Cherry: {}, Strawberry: {} },
+    Yellow: { Banana: {}, Pineapple: {} },
   },
 };
 
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(li, data[key]);
+    }
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
