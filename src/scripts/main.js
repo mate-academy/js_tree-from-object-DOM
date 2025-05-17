3454d3ad55ade71a9ba3b84f9f8073f6b1944daf
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-17 16:59:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,25 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const tree = document.createElement('ul');
+
+tree.id = 'tree';
+document.body.appendChild(tree);
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    element.appendChild(li);
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
+      const subUl = document.createElement('ul');
+
+      li.appendChild(subUl);
+      createTree(subUl, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
