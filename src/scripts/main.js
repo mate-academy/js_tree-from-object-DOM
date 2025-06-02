020db8a7758f4a872b59630dc0dffe357018724f
Khrystyna Melnyk <kristinamelnik50@gmail.com>
2025-06-02 16:51:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,25 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  if (!data || typeof data !== 'object') {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (data[key] && typeof data[key] === 'object') {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
