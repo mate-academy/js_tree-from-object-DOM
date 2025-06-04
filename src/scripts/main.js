ddc5554f0859d181ae609d4fd31619f74cd07960
Alexey Kravets <kravecoleksiy@gmail.com>
2025-06-04 17:23:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,23 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const ulElement = document.createElement('ul');
+
+  for (const key in data) {
+    const liElement = document.createElement('li');
+
+    liElement.textContent = key;
+
+    createTree(liElement, data[key]);
+
+    ulElement.appendChild(liElement);
+  }
+
+  element.appendChild(ulElement);
 }
 
 createTree(tree, food);
