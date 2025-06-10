c6362f55f2eb2bb365d3d8e7ee759064025603e0
BeginerAlex <aleksander9677@gmail.com>
2025-06-10 18:11:55 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,28 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!data || typeof data !== 'object') {
+    return null;
+  }
+
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const listItem = document.createElement('li');
+
+      listItem.textContent = key;
+      list.appendChild(listItem);
+
+      if (typeof data[key] === 'object' && data[key] !== null) {
+        createTree(listItem, data[key]);
+      }
+
+      list.appendChild(listItem);
+    }
+  }
+
+  element.appendChild(list);
 }
 
 createTree(tree, food);
