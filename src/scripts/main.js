6e19a6266255ba13d30b94777d3cccc97e4bcd77
Sergii Nosachenko <sergii.n@mate.academy>
2025-07-21 12:35:39 +0300

    Update README.md

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,25 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const iterator = (foods) => {
+    const keys = Object.keys(foods);
+
+    if (keys.length === 0) {
+      return '';
+    }
+
+    return keys
+      .map((key) => {
+        if (Object.keys(foods[key]).length > 0) {
+          return `<li>${key}<ul>${iterator(foods[key])}</ul></li>`;
+        }
+
+        return `<li>${key}</li>`;
+      })
+      .join('');
+  };
+
+  element.innerHTML = `<ul>${iterator(data)}</ul>`;
 }
 
 createTree(tree, food);
