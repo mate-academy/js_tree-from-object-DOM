8834cd0254b55edee939030333a7c2f59719b1bd
Nazar <nazar.shevchuk.121@gmail.com>
2025-07-27 12:25:58 +0300

    sol1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const { createElement } = require('react');
+
 const food = {
   Drink: {
     Wine: {},
@@ -18,10 +20,26 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const tree = document.createElement('div');
 
+tree.id = 'tree';
+document.body.appendChild(tree);
+
+// document.body.createElement(tree);
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+    ul.appendChild(li);
+  }
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
