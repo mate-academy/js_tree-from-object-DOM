c2190aa43f1e57adffbf1682f5d7dafafca69c0f
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-07-29 22:58:15 +0300

    fix recursion function to empty object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,7 +33,9 @@ function createTree(element, data) {
 
     li.textContent = key;
 
-    createTree(li, data[key]);
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
 
     ul.appendChild(li);
   }
