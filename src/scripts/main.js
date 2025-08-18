92842aef81b518c7df69e5af73ad302df5a315ce
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-18 19:18:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,21 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+
+    list.appendChild(li);
+  }
+
+  element.appendChild(list);
 }
 
 createTree(tree, food);
