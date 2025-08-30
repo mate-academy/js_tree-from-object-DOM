04c757c3b74442f9c85843fdcf04d53bef481259
Dmytrii <dimon.gorovoi33@gmail.com>
2025-08-30 18:07:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,27 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!data || typeof data !== 'object') {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (data[key] && Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  }
+
+  if (ul.children.length > 0) {
+    element.appendChild(ul);
+  }
 }
 
 createTree(tree, food);
