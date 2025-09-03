6a71f35461a18000ef7b72a7400ebbb15f1756f2
annakalmazan <klavachichikova@gmail.com>
2025-09-03 20:02:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,26 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const tree = document.querySelector('div');
+
+// tree.id = 'tree';
+/// document.body.appendChild(tree);
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.appendChild(li);
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+  }
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
