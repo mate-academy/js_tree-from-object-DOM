4c392b567a9bcf7bcadcbc938ed62849392714af
S-Khraban <s.hraban@i.ua>
2025-09-18 18:54:15 +0300

    _Create Tree of obj_

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,34 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+
+  function buildList(obj) {
+    if (!obj || Object.keys(obj).length === 0) {
+      return null;
+    }
+
+    const ul = document.createElement('ul');
+
+    for (const key in obj) {
+      const li = document.createElement('li');
+      li.textContent = key;
+
+      const childrenUl = buildList(obj[key]);
+      if (childrenUl) {
+        li.appendChild(childrenUl);
+      }
+
+      ul.appendChild(li);
+    }
+
+    return ul;
+  }
+
+  const ul = buildList(data);
+
+  if (ul) {
+    element.appendChild(ul);
+  }
 }
 
 createTree(tree, food);
