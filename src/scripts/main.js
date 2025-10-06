65b9a45d0f25e8830a844b1a292d2b4188e3f22e
Yaroslav Molchanov <y.molchanoff@gmail.com>
2025-10-06 22:53:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,27 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  // Якщо об’єкт порожній — нічого не створюємо
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    // Якщо є вкладені елементи — викликаємо рекурсію
+    if (Object.keys(data[key]).length) {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
