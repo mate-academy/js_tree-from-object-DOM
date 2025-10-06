5c5e6f4980248c9dbef2d5b194de4343c926c51b
SheFing <shevchukm2002@gmail.com>
2025-10-06 17:32:42 +0300

    att1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,27 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!element) {
+    return;
+  }
+
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    createTree(li, data[key]);
+
+    ul.append(li);
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
