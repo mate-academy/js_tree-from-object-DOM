7b98556f91f34168039f6ce6190de6a1d05b193d
Dmytro Popov <lort20178@gmail.com>
2025-10-24 09:36:31 +0300

    Add second solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,7 +32,11 @@ function createTree(element, data) {
 
     li.textContent = key;
 
-    if (Object.keys(data[key]).length > 0) {
+    if (
+      data[key] &&
+      typeof data[key] === 'object' &&
+      Object.keys(data[key]).length > 0
+    ) {
       createTree(li, data[key]);
     }
 
