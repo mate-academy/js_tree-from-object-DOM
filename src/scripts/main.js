6cf622cd7a337d8412304440507965947bf36e26
Yurii Drutsul <druculurij@gmail.com>
2025-11-11 15:39:27 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,8 +20,33 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
+function hasNestedData(obj) {
+  const isObject = typeof obj === 'object' && obj !== null;
+  const hasKeys = Object.keys(obj).length > 0;
+
+  return isObject && hasKeys;
+}
+
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+  const keys = Object.keys(data);
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    const value = data[key];
+
+    if (hasNestedData(value)) {
+      createTree(li, value);
+    }
+
+    ul.append(li);
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
