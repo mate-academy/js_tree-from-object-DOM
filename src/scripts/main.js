44b020bd702558afb4db5754dd84c8bcb9e99475
Andrii Tsopych <andriythoupich01@gmail.com>
2026-01-05 16:49:16 +0200

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,25 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  if (Object.keys(data).length === 0) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (data[key] !== null) {
+      createTree(li, data[key]);
+    }
+
+    ul.append(li);
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
