2654dc7a4371b15ec158ed6452893e61174eac8b
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-02-12 23:03:07 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,6 @@ const food = {
     Wine: {},
     Schnaps: {},
   },
-
   Fruit: {
     Red: {
       Cherry: {},
@@ -21,7 +20,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!data) {
+    return element;
+  }
+
+  const list = document.createElement('ul');
+
+  for (const [key, value] of Object.entries(data)) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    createTree(li, value);
+
+    list.append(li);
+  }
+
+  element.append(list);
+
+  return list;
 }
 
 createTree(tree, food);
