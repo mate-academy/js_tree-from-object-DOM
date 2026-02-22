c18c56273a7e430fb2f1cc0e224f670ee9f0f355
Volodymyr Shuper <vshuper@gmail.com>
2026-02-22 18:16:36 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,6 +22,29 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const li = document.createElement('li');
+
+      li.textContent = key;
+
+      const value = data[key];
+
+      if (
+        typeof value === 'object' &&
+        value !== null &&
+        Object.keys(value).length > 0
+      ) {
+        createTree(li, value);
+      }
+
+      ul.append(li);
+    }
+  }
+
+  element.append(ul);
 }
 
 createTree(tree, food);
