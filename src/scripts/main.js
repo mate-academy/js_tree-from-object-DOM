c684ec302a9b2e39eb0d972baa46b204e1476b90
Oleksandr Shyshenok <shyshenok81@gmail.com>
2026-03-04 23:46:27 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,32 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (!data.hasOwnProperty(key)) {
+      continue;
+    }
+
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    const value = data[key];
+
+    if (
+      typeof value === 'object' &&
+      value !== null &&
+      Object.keys(value).length > 0
+    ) {
+      createTree(li, value);
+    }
+
+    ul.appendChild(li);
+  }
+
+  // 5️⃣ Додаємо готовий список в element
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
