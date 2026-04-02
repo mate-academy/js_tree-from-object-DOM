e691c00ae6769ed13404afba9286c204f552675f
Alternate001 <artem.kushko@publizer.de>
2026-04-02 11:59:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  function branch(list, nestedList = element) {
+    for (const key in list) {
+      const root = document.createElement('li');
+
+      root.textContent = key;
+      nestedList.appendChild(root);
+
+      if (list[key] && typeof list[key] === 'object') {
+        const splitRoot = document.createElement('ul');
+
+        if (Object.keys(list[key]).length > 0) {
+          root.appendChild(splitRoot);
+        }
+        branch(list[key], splitRoot);
+      }
+    }
+  }
+
+  branch(data);
 }
 
 createTree(tree, food);
