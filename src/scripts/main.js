7c04ec17feaf09ed8dcd27b181e802c6639f0299
Vita Martsekhovska <marcehovskavita@gmail.com>
2026-05-21 17:37:05 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (Object.keys(data).length === 0) {
+    return;
+  }
+
+  const ourUl = document.createElement('ul');
+
+  for (const key in data) {
+    const ourLi = document.createElement('li');
+
+    ourLi.textContent = key;
+    createTree(ourLi, data[key]);
+    ourUl.append(ourLi);
+  }
+
+  element.append(ourUl);
 }
 
 createTree(tree, food);
