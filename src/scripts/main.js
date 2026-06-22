5c0d5ff58f851abded734aed3dd4adedb8b4bf3e
Ihor Balakhmei <balakhmei.dev@gmail.com>
2026-06-22 23:32:04 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    element.appendChild(li);
+    li.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      const ul = document.createElement('ul');
+
+      li.appendChild(ul);
+      createTree(ul, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
