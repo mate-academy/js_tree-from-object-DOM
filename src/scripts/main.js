e88522c99b6d0e804e560317333398412bb384eb
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-23 21:27:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,8 +20,21 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
-function createTree(element, data) {
-  // df
+function createTree(table, obj) {
+  const ul = document.createElement('ul');
+
+  for (const key in obj) {
+    const value = obj[key];
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (value !== null && typeof value === 'object') {
+      createTree(li, value);
+    }
+    ul.appendChild(li);
+  }
+  table.appendChild(ul);
 }
 
 createTree(tree, food);
