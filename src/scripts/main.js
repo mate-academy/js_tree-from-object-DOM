c9fc0fb490ad4c81b64e76f61141992f4c4ef7d2
Yelyzaveta Romanovska <lizarom2608@gmail.com>
2026-07-24 17:48:03 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,32 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (!data) {
+    return;
+  }
+
+  let treeBranch = element;
+
+  if (!element) {
+    treeBranch = document.createElement('div');
+    treeBranch.id = 'tree';
+    document.body.prepend(treeBranch);
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const dataElem in data) {
+    const li = document.createElement('li');
+
+    li.append(dataElem);
+
+    createTree(li, data[dataElem]);
+    ul.append(li);
+  }
+
+  if (ul.innerHTML) {
+    treeBranch.append(ul);
+  }
 }
 
 createTree(tree, food);
