3ae96cbb74ec5fd5358b799d25b7cb2e373282fa
nataliandr <fnnny.yann@mail.com>
2026-08-01 22:25:17 +0200

    tree from dom solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,20 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  Object.keys(data).forEach((key) => {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+  });
+
+  element.append(ul);
 }
 
 createTree(tree, food);
