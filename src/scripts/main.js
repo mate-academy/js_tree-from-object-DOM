cb131ef34ae4f8d3e5bfd865e2ec43fb8c42d359
Marichka <maria_volynchuk@ukr.net>
2026-08-10 21:52:45 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const mainUl = document.createElement('ul');
+
+  element.insertAdjacentElement('beforeend', mainUl);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.append(key);
+
+    mainUl.insertAdjacentElement('beforeend', li);
+
+    if (Object.keys(data[key]).length) {
+      createTree(mainUl, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
