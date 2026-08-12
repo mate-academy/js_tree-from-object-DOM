b9c57dcc77ecb273c4e13e532e96910d5c8ebb5e
PrinceValue <princeossoa@gmail.com>
2026-08-12 11:16:05 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,31 @@ const food = {
   },
 };
 
+const newtree = document.createElement('div');
+
+newtree.id = 'tree';
+document.body.appendChild(newtree);
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  Object.keys(data).forEach((key) => {
+    const li = document.createElement('li');
+
+    const hasChildren = Object.keys(data[key]).length > 0;
+
+    li.textContent = key;
+
+    if (hasChildren) {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  });
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
