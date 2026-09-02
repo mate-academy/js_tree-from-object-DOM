81731d77516db6e9d6a196ffa943b8c1ed25d4dc
Elvira Petrenko <rukola2008_r@users.noreply.github.com>
2026-09-02 22:13:57 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,27 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const tree = document.createElement('div');
+
+tree.id = 'tree';
+document.body.appendChild(tree);
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  Object.keys(data).forEach((key) => {
+    const item = document.createElement('li');
+
+    item.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(item, data[key]);
+    }
+
+    list.appendChild(item);
+  });
+
+  element.appendChild(list);
 }
 
 createTree(tree, food);
